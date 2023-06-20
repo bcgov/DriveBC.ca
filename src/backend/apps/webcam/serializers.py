a7712b8dd6bfd5b7def4ee0fc4acadee8c77cb29
ray <ray@oxd.com>
2023-06-20 14:02:18 -0700

    DBC22-439: added link for unedited, last working source image

--- src/backend/apps/webcam/serializers.py
+++ src/backend/apps/webcam/serializers.py
@@ -1,6 +1,5 @@
-from rest_framework import serializers
-
 from apps.webcam.models import Webcam
+from rest_framework import serializers
 
 
 class WebcamSerializer(serializers.ModelSerializer):
@@ -8,15 +7,21 @@ class WebcamSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Webcam
-        exclude = ('created_at', 'modified_at', )
+        exclude = (
+            "created_at",
+            "modified_at",
+        )
 
     def get_links(self, obj):
+        api_root = "https://images.drivebc.ca/"
         webcam_id = obj.id
+
         links = {
-            "imageDisplay": f"https://images.drivebc.ca/bchighwaycam/pub/cameras/{webcam_id}.jpg",
-            "imageThumbnail": f"https://images.drivebc.ca/bchighwaycam/pub/cameras/tn/{webcam_id}.jpg",
-            "currentImage": f"https://images.drivebc.ca/webcam/imageUpdate.php?cam={webcam_id}",
-            "replayTheDay": f"https://images.drivebc.ca/ReplayTheDay/player.html?cam={webcam_id}"
+            "imageSource": f"{api_root}webcam/api/v1/webcams/{webcam_id}/imageSource",
+            "imageDisplay": f"{api_root}bchighwaycam/pub/cameras/{webcam_id}.jpg",
+            "imageThumbnail": f"{api_root}bchighwaycam/pub/cameras/tn/{webcam_id}.jpg",
+            "currentImage": f"{api_root}webcam/imageUpdate.php?cam={webcam_id}",
+            "replayTheDay": f"{api_root}ReplayTheDay/player.html?cam={webcam_id}",
         }
 
         return links
