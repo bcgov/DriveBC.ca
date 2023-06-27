1fcb2223c13f9518b5b36791692870af5c2173c0
Tyrel Narciso <107274013+tyrel-oxd@users.noreply.github.com>
2023-06-27 14:46:53 -0700

    DBC22-433: Added camera details page and routing (#12)
    Note, currently there are still a few implementation items still required at this stage: 
    
    -Implementation of "Next refresh attempt time"
    -Implementation of "Replay the day"
    -Implementation of "Map view"
    -Implementation/Database of highway aliases

--- src/frontend/src/Components/cameras/WebcamCard.js
+++ src/frontend/src/Components/cameras/WebcamCard.js
@@ -1,15 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import './WebcamCard.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faMapMarkerAlt 
+  faMapMarkerAlt
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function WebcamCard({ camera }) {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/CameraDetailsPage", { state: camera.id})
+  }
   return (
     <Card className="webcam-card">
-      <Card.Body>
+      <Card.Body onClick={handleClick}>
         <div className="card-img-box">
           <Card.Img variant="top" src={camera.properties.url} />
         </div>
