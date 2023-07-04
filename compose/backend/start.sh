c807c0fee6ced095494f484b58afce1e947699b6
Justin Johnson <justin@oxd.com>
2023-07-04 15:04:20 -0700

    Update dev.yaml

--- compose/backend/start.sh
+++ compose/backend/start.sh
@@ -4,11 +4,14 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+echo 'migrating'
 python manage.py migrate
 
+echo 'migration done; creating superuser'
 python manage.py createsuperuser \
     --noinput \
     --username $DJANGO_SUPERUSER_USERNAME \
-    --email $DJANGO_SUPERUSER_EMAIL || true
+    --email $DJANGO_SUPERUSER_EMAIL || true 2> /dev/null
 
+echo 'creating superuser done; starting service'
 python manage.py runserver 0.0.0.0:8000
