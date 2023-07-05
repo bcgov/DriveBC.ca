2832a939cc9b01092edeef0c9e25af88cd67956c
ray <ray@oxd.com>
2023-07-05 16:58:04 -0700

    DBC22-593: working api endpoint, added management command to populate

---
+++ src/backend/apps/event/views.py
@@ -0,0 +1,18 @@
+from apps.event.models import Event
+from apps.event.serializers import EventSerializer
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+
+class EventViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+    def list(self, request, *args, **kwargs):
+        # Use pagination only when offset or limit is in querystring
+        if "limit" in request.query_params and "offset" in request.query_params:
+            return super().list(request, *args, **kwargs)
+
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({"results": serializer.data})
