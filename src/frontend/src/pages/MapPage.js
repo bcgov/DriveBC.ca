b044044171b78c571bafd12f3b63b088be415448
Tyrel Narciso <107274013+tyrel-oxd@users.noreply.github.com>
2023-07-12 11:36:05 -0700

    DBC22-597: Code cleanup and initial map window (#28)

--- src/frontend/src/pages/MapPage.js
+++ src/frontend/src/pages/MapPage.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { DndProvider } from 'react-dnd-multi-backend';
 import { HTML5toTouch } from 'rdndmb-html5-to-touch';
-
+import "../Components/Map.scss"
 import Map from '../Components/Map.js';
 
 export default function MapPage() {
   return (
     <DndProvider options={HTML5toTouch}>
-      <Map />
+      <div className="map-wrap">
+      <Map/>
+      </div>
     </DndProvider>
   );
 }
