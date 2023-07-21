f86e25a5c29559edca38c2f07e5510abe0ada226
Tyrel Narciso <107274013+tyrel-oxd@users.noreply.github.com>
2023-07-21 15:31:01 -0700

    DBC22-629: Fixed pagination and changed map zoom (#40)

--- src/frontend/src/Components/events/EventsTable.js
+++ src/frontend/src/Components/events/EventsTable.js
@@ -1,86 +1,86 @@
-import React, { useState, useMemo } from "react";
+import React, { useState } from "react";
 import {
   flexRender,
   getCoreRowModel,
   getSortedRowModel,
   getPaginationRowModel,
   useReactTable,
-} from '@tanstack/react-table'
-import "./EventsTable.scss"
+} from "@tanstack/react-table";
+import "./EventsTable.scss";
 
+export default function EventsTable({ columns, data }) {
+  const [sorting, setSorting] = useState([]);
 
-export default function EventsTable({columns, data}) {
-const columnData = useMemo(() => columns, [columns]);
-const rowData = useMemo(() => data, [data]);
-const [sorting, setSorting] = useState([]);
-
-const table = useReactTable(
-    {
-      data: rowData,
-      columns: columnData,
-      state: {
-        sorting,
-      },
-      onSortingChange: setSorting,
-      getPaginationRowModel: getPaginationRowModel(),
-      getCoreRowModel: getCoreRowModel(),
-      getSortedRowModel: getSortedRowModel(),
+  const table = useReactTable({
+    data: data,
+    columns: columns,
+    state: {
+      sorting,
     },
-  );
-return (
+    initialState: {
+      pagination: {
+        pageSize: -1,
+      },
+    },
+    onSortingChange: setSorting,
+    getPaginationRowModel: getPaginationRowModel(),
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+  });
+  return (
     <table>
-        <thead>
-          {table.getHeaderGroups().map(headerGroup => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map(header => {
-                return (
-                  <th key={header.id} colSpan={header.colSpan}>
-                    {header.isPlaceholder ? null : (
-                      <div
-                        {...{
-                          className: header.column.getCanSort()
-                            ? 'cursor-pointer select-none'
-                            : '',
-                          onClick: header.column.getToggleSortingHandler(),
-                        }}
-                      >
-                        {flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                        {{
-                          asc: ' 🔼',
-                          desc: ' 🔽',
-                        }[header.column.getIsSorted()] ?? null}
-                      </div>
-                    )}
-                  </th>
-                )
-              })}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table
-            .getRowModel()
-            .rows.slice(0, 10)
-            .map(row => {
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => {
               return (
-                <tr key={row.id}>
-                  {row.getVisibleCells().map(cell => {
-                    return (
-                      <td key={cell.id}>
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext()
-                        )}
-                      </td>
-                    )
-                  })}
-                </tr>
-              )
+                <th key={header.id} colSpan={header.colSpan}>
+                  {header.isPlaceholder ? null : (
+                    <div
+                      {...{
+                        className: header.column.getCanSort()
+                          ? "cursor-pointer select-none"
+                          : "",
+                        onClick: header.column.getToggleSortingHandler(),
+                      }}
+                    >
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                      {{
+                        asc: " 🔼",
+                        desc: " 🔽",
+                      }[header.column.getIsSorted()] ?? null}
+                    </div>
+                  )}
+                </th>
+              );
             })}
-        </tbody>
-      </table>
-)
-        }
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table
+          .getRowModel()
+          .rows
+          .map((row) => {
+            return (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <td key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+      </tbody>
+    </table>
+  );
+}
