7ff76e4e2b5c21c0caa203ec53df5e5a0847dac1
ray <ray@oxd.com>
2023-08-21 14:32:02 -0700

    DBC22-684: fixed header menu not collapsing on selection

--- src/frontend/src/Header.js
+++ src/frontend/src/Header.js
@@ -1,38 +1,56 @@
-import React from 'react';
-import './Header.scss';
+// React
+import React, { useState } from "react";
+
+// Third party packages
+import {LinkContainer} from 'react-router-bootstrap';
+import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import logo from './images/BCID_H_rgb_pos.png';
-import Container from 'react-bootstrap/Container';
-import {LinkContainer} from 'react-router-bootstrap';
 
+// Static files
+import logo from './images/BCID_H_rgb_pos.png';
+
+// Styling
+import './Header.scss';
 
 export default function Header() {
+  // State hooks
+  const [expanded, setExpanded] = useState(false);
+
+  // Component functions
+  const getNavLink = (title) => {
+    return <Nav.Link active={false} onClick={() => setTimeout(() => setExpanded(false))}>{title}</Nav.Link>
+  };
+
+  // Rendering
   return (
     <header className="header--shown">
-      <Navbar expand="lg">
+      <Navbar expand="lg" expanded={expanded}>
         <Container>
-          <Navbar.Toggle>
+          <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")}>
             <span className="line line1"></span>
             <span className="line line2"></span>
             <span className="line line3"></span>
           </Navbar.Toggle>
+
           <Navbar.Brand href="#home">
             <a href="https://gov.bc.ca" className="header-link">
               <img className="header-logo" src={logo} alt="Government of British Columbia" />
             </a>
           </Navbar.Brand>
+
           <div className="nav-divider"></div>
+
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <LinkContainer exact to="/">
-                <Nav.Link active={false}>Map</Nav.Link>
+                {getNavLink('Map')}
               </LinkContainer>
               <LinkContainer exact to="/cameras-page">
-                <Nav.Link active={false}>Cameras</Nav.Link>
+                {getNavLink('Cameras')}
               </LinkContainer>
               <LinkContainer exact to="/events-page">
-                <Nav.Link active={false}>Delays</Nav.Link>
+                {getNavLink('Delays')}
               </LinkContainer>
             </Nav>
           </Navbar.Collapse>
