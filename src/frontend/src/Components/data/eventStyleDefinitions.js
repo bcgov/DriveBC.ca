33c874ef24f0280c38f155b9ad53617dae252ec3
minORC <min@openroad.ca>
2023-08-22 22:09:33 -0700

    DBC22-596: Map icons for cameras and events static, hover, and active states

--- src/frontend/src/Components/data/eventStyleDefinitions.js
+++ src/frontend/src/Components/data/eventStyleDefinitions.js
@@ -1,27 +1,72 @@
 import {Icon, Stroke, Style} from 'ol/style.js';
-import eventIcon from '../../assets/exclamation-triangle-solid.png';
-import videoIcon from '../../assets/video-solid.png';
+import cameraIconActive from "../../images/camera-active.svg";
+import cameraIconHover from "../../images/camera-hover.svg";
+import cameraIconStatic from "../../images/camera-static.svg";
+import eventIconActive from "../../images/incident-active.svg";
+import eventIconHover from "../../images/incident-hover.svg";
+import eventIconStatic from "../../images/incident-static.svg";
 
 
-export const webcamStyles = {
-  default: new Style({
+// Camera icon styles
+export const cameraStyles = {
+  static: new Style({
     image: new Icon({
       anchor: [24, 24],
       anchorXUnits: 'pixels',
       anchorYUnits: 'pixels',
-      scale: 0.5,
-      src: videoIcon,
+      scale: 1,
+      src: cameraIconStatic,
+    }),
+  }),
+  hover: new Style({
+    image: new Icon({
+      anchor: [24, 24],
+      anchorXUnits: 'pixels',
+      anchorYUnits: 'pixels',
+      scale: 1,
+      src: cameraIconHover,
+    }),
+  }),
+  active: new Style({
+    image: new Icon({
+      anchor: [24, 24],
+      anchorXUnits: 'pixels',
+      anchorYUnits: 'pixels',
+      scale: 1,
+      src: cameraIconActive,
     }),
   }),
 };
+
+// Event icon styles
 export const eventStyles = {
-  Points: new Style({
+  static: new Style({
     image: new Icon({
       anchor: [24, 24],
       anchorXUnits: 'pixels',
       anchorYUnits: 'pixels',
-      scale: 0.5,
-      src: eventIcon,
+      scale: 1,
+      src: eventIconStatic,
+    }),
+  }),
+
+  hover: new Style({
+    image: new Icon({
+      anchor: [24, 24],
+      anchorXUnits: 'pixels',
+      anchorYUnits: 'pixels',
+      scale: 1,
+      src: eventIconHover,
+    }),
+  }),
+
+  active: new Style({
+    image: new Icon({
+      anchor: [24, 24],
+      anchorXUnits: 'pixels',
+      anchorYUnits: 'pixels',
+      scale: 1,
+      src: eventIconActive,
     }),
   }),
 
@@ -31,6 +76,5 @@ export const eventStyles = {
       width: 8,
     }),
   }),
-
 
 };
