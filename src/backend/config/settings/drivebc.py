5618572e75019c18b0c46fd9f2dfcbd4cac982dd
ray <ray@oxd.com>
2023-08-25 13:11:35 -0700

    DBC22-849: env variables for API urls

--- src/backend/config/settings/drivebc.py
+++ src/backend/config/settings/drivebc.py
@@ -8,7 +8,7 @@ env = environ.Env()
 environ.Env.read_env(BASE_DIR / '.env', overwrite=True)
 
 # Drive BC API Settings
-DRIVEBC_ROUTE_PLANNER_API_BASE_URL = "https://router.api.gov.bc.ca/"
+DRIVEBC_ROUTE_PLANNER_API_BASE_URL = env("DRIVEBC_ROUTE_PLANNER_API_BASE_URL")
 DRIVEBC_ROUTE_PLANNER_API_AUTH_KEY = env("DRIVEBC_ROUTE_PLANNER_API_AUTH_KEY")
-DRIVEBC_WEBCAM_API_BASE_URL = "https://images.drivebc.ca/webcam/api/v1/"
-DRIVEBC_OPEN_511_API_BASE_URL = "https://api.open511.gov.bc.ca/"
+DRIVEBC_WEBCAM_API_BASE_URL = env("DRIVEBC_WEBCAM_API_BASE_URL")
+DRIVEBC_OPEN_511_API_BASE_URL = env("DRIVEBC_OPEN_511_API_BASE_URL")
