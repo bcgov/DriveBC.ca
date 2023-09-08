04844535bd19c30190405c3f38e9ed3d6f2d020d
bcgov-brwang <bcgov.brwang@gmail.com>
2023-09-08 12:34:39 -0700

    DBC22-928: added advisory model and api end point
    Added advisories and create API endpoints for the FE to consume.

--- src/backend/apps/cms/views.py
+++ src/backend/apps/cms/views.py
@@ -1,9 +1,14 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from .models import TestCMSData
-from .serializers import FAQSerializer
+from .models import TestCMSData, Advisory
+from .serializers import FAQSerializer, AdvisorySerializer
 
 
 class FAQAPIViewSet(ReadOnlyModelViewSet):
     queryset = TestCMSData.objects.filter(live=True)
     serializer_class = FAQSerializer
+
+
+class AdvisoryAPIViewSet(ReadOnlyModelViewSet):
+    queryset = Advisory.objects.filter(live=True)
+    serializer_class = AdvisorySerializer
