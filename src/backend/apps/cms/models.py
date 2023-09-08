e5e22c058bdaeea8392cbdafe3ec25b6ab54c779
bcgov-brwang <bcgov.brwang@gmail.com>
2023-09-08 12:34:39 -0700

    DBC22-928: added advisory model and api end point
    Added advisories and create API endpoints for the FE to consume.

--- src/backend/apps/cms/models.py
+++ src/backend/apps/cms/models.py
@@ -6,6 +6,10 @@ from wagtail.fields import RichTextField
 from wagtail.models import DraftStateMixin, RevisionMixin
 from wagtail.search import index
 from wagtail.snippets.models import register_snippet
+from wagtail.models import Page
+from wagtail.api import APIField
+from wagtail.templatetags import wagtailcore_tags
+# from django import forms
 
 
 class DriveBCMapWidget(OSMWidget):
@@ -43,3 +47,51 @@ class TestCMSData(DraftStateMixin, RevisionMixin, index.Indexed, BaseModel):
         FieldPanel("url"),
         FieldPanel("location_geometry", widget=DriveBCMapWidget),
     ]
+
+
+class Advisory(Page, BaseModel):
+    page_description = "Use this page for creating advisories"
+    # Title field for the advisory
+    advisory_title = models.CharField(max_length=255)
+    advisory_active = models.BooleanField(default=True)
+
+    # Teaser field for a brief description
+    # advisory_teaser = models.TextField()
+
+    # Rich text field for the body content
+    advisory_description = RichTextField(blank=True)
+    
+    def rendered_description(self):
+        return wagtailcore_tags.richtext(self.advisory_description)
+
+    api_fields = [
+        APIField('rendered_description'),
+    ]
+
+    # Geo fields
+    advisory_geometry = models.GeometryField()
+
+    def save(self, *args, **kwargs):
+        self.title = self.advisory_title
+        super().save(*args, **kwargs)
+
+    # Editor panels configuration
+    content_panels = [
+        # FieldPanel("page_ptr"),
+        # FieldPanel("title"),
+        # FieldPanel("depth"),
+        # FieldPanel("path"),
+        # FieldPanel("numchild"),
+        FieldPanel("advisory_title"),
+        FieldPanel("advisory_active"),
+        # FieldPanel("advisory_teaser"),
+        FieldPanel("advisory_description"),
+        FieldPanel("advisory_geometry", widget=DriveBCMapWidget),
+    ]
+    promote_panels = []
+
+    # Parent page / subpage type rules
+    # parent_page_types = ['wagtailcore.Page']
+    # subpage_types = []
+
+    template = 'cms/advisory.html'
