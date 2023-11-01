2b91c783cd5b9918ac14c854eb813e942afefffa
minORC <min@openroad.ca>
2023-11-01 14:10:27 -0700

    DBC22-1084: Removing placeholder for social sharing

---
+++ src/frontend/src/Components/SocialSharing.js
@@ -0,0 +1,38 @@
+// React
+import React from 'react';
+
+// Third party packages
+import Container from 'react-bootstrap/Container';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {
+  faEnvelope
+} from '@fortawesome/free-regular-svg-icons';
+import {
+  faXTwitter,
+  faInstagram,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
+
+export default function SocialSharing() {
+  return (
+  <Container className="social-share-container">
+    <div className="social-share-div">
+      <p className="bold hero">Share this page</p>
+      <div className="social-share">
+        <a href="https://twitter.com/DriveBC" className="footer-link" target="_blank" rel="noreferrer"  alt="Twitter">
+          <FontAwesomeIcon icon={faXTwitter} />
+        </a>
+        <a href="https://www.instagram.com/ministryoftranbc/" className="footer-link" target="_blank" rel="noreferrer"  alt="Instagram">
+          <FontAwesomeIcon icon={faInstagram}/>
+        </a>
+        <a href="https://www.linkedin.com/company/british-columbia-ministry-of-transportation-and-infrastructure/" className="footer-link" target="_blank" rel="noreferrer" alt="Linkedin" >
+          <FontAwesomeIcon icon={faLinkedin}/>
+        </a>
+        <a href="" className="footer-link" target="_blank" rel="noreferrer" alt="Email" >
+          <FontAwesomeIcon icon={faEnvelope}/>
+        </a>
+      </div>
+    </div>
+  </Container>
+  );
+}
