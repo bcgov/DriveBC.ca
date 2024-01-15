828619beffe0fc20c0b71fac1a06e0c737e6a86e
ray <ray@oxd.com>
2024-01-15 11:24:25 -0800

    DBC22-1302: moved cam popup into its own component

--- src/frontend/src/Components/map/mapPopup.js
+++ src/frontend/src/Components/map/mapPopup.js
@@ -7,8 +7,6 @@ import EventTypeIcon from '../EventTypeIcon';
 import FriendlyTime from '../FriendlyTime';
 import parse from 'html-react-parser';
 
-import colocatedCamIcon from '../../images/colocated-camera.svg';
-
 const displayCategoryMap = {
   closures: 'Closure',
   majorEvents: 'Major Delay',
@@ -34,67 +32,6 @@ function convertDirection(direction) {
       default:
           return " ";
   }
-}
-
-function renderCamGroup(camFeature, setClickedCamera, currentCamData) {
-  const rootCamData = camFeature.ol_uid ? camFeature.getProperties() : camFeature;
-
-  const clickHandler = (i) => {
-    camFeature.setProperties({ groupIndex: i });
-    setClickedCamera(camFeature);  // Trigger re-render
-  }
-
-  const res = Object.entries(rootCamData.camGroup).map(([index, cam]) => {
-    return (
-      <Button className={'camera-direction-btn' + ((currentCamData.orientation == cam.orientation) ? ' current' : '') }
-       key={cam.id} onClick={(event) => {event.stopPropagation(); clickHandler(index)}}>
-        {cam.orientation}
-      </Button>
-    );
-  });
-
-  return res;
-}
-
-export function getCamPopup(camFeature, setClickedCamera, navigate, cameraPopupRef, isPreview) {
-  const rootCamData = camFeature.id ? camFeature : camFeature.getProperties();
-  const camData = !rootCamData.groupIndex ? rootCamData : rootCamData.camGroup[rootCamData.groupIndex];
-
-  const handlePopupClick = (e) => {
-    if (!cameraPopupRef.current && !isPreview) {
-      navigate(`/cameras/${camData.id}`);
-      cameraPopupRef.current = null;
-    }
-  };
-
-  return (
-    <div className="popup popup--camera">
-      <div onClick={handlePopupClick}>
-        <div className="popup__title">
-          <p className="bold name">{camData.name}</p>
-          <p className="bold orientation">{camData.orientation}</p>
-        </div>
-
-        <div className="popup__description">
-          <p>{parse(camData.caption)}</p>
-          <div className="popup__camera-info">
-            <div className="camera-orientations">
-              <img className="colocated-camera-icon" src={colocatedCamIcon} role="presentation" alt="colocated cameras icon" />
-              {renderCamGroup(camFeature, setClickedCamera, camData)}
-            </div>
-            <div className="camera-image">
-              <img src={camData.links.imageSource} width='300' />
-
-              <div className="timestamp">
-                <p className="driveBC">Drive<span>BC</span></p>
-                <FriendlyTime date={camData.last_update_modified} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
 }
 
 export function getEventPopup(eventFeature) {
