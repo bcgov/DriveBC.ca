3938d897379b61fc6c59ca25b6a2eec750d60688
ray <ray@oxd.com>
2024-01-15 16:47:25 -0800

    DBC22-1302: reworked cam popup component

--- src/frontend/src/Components/map/camPopup.js
+++ src/frontend/src/Components/map/camPopup.js
@@ -1,5 +1,8 @@
 // React
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
+// Navigation
+import { useNavigate } from 'react-router-dom';
 
 // Third party packages
 import Button from 'react-bootstrap/Button';
@@ -8,67 +11,90 @@ import parse from 'html-react-parser';
 
 import colocatedCamIcon from '../../images/colocated-camera.svg';
 
-
-function renderCamGroup(camFeature, setClickedCamera, currentCamData) {
-  const rootCamData = camFeature.ol_uid ? camFeature.getProperties() : camFeature;
-
-  const clickHandler = (i) => {
-    console.log('test');
-    camFeature.setProperties({ groupIndex: i });
-    setClickedCamera(camFeature);  // Trigger re-render
-  }
-
-  const res = Object.entries(rootCamData.camGroup).map(([index, cam]) => {
-    return (
-      <Button className={'camera-direction-btn' + ((currentCamData.orientation == cam.orientation) ? ' current' : '') }
-       key={cam.id} onClick={(event) => {event.stopPropagation(); clickHandler(index)}}>
-        {cam.orientation}
-      </Button>
-    );
-  });
-
-  return res;
-}
-
 export default function CamPopup(props) {
-  const { camFeature, setClickedCamera, navigate, cameraPopupRef, isPreview } = props;
+  // Props
+  const { camFeature, isPreview } = props;
 
-  const rootCamData = camFeature.id ? camFeature : camFeature.getProperties();
-  const camData = !rootCamData.groupIndex ? rootCamData : rootCamData.camGroup[rootCamData.groupIndex];
+  // Misc
+  const navigate = useNavigate();
 
+  // Refs
+  const isInitialMount = useRef(true);
+
+  // useState hooks
+  const [rootCam, setRootCam] = useState();
+  const [camera, setCamera] = useState();
+  const [camIndex, setCamIndex] = useState(0);
+
+  // useEffect hooks
+  useEffect(() => {
+    const newCam = camFeature.id ? camFeature : camFeature.getProperties();
+    setRootCam(newCam);
+    setCamera(newCam);
+  }, [camFeature]);
+
+  useEffect(() => {
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+      return;
+    }
+
+    setCamera(rootCam.camGroup[camIndex]);
+  }, [camIndex]);
+
+  // Handlers
   const handlePopupClick = (e) => {
-    if (!cameraPopupRef.current && !isPreview) {
-      navigate(`/cameras/${camData.id}`);
-      cameraPopupRef.current = null;
+    if (!isPreview) {
+      navigate(`/cameras/${camera.id}`);
     }
   };
 
+  // Rendering
+  function renderCamGroup(currentCamData) {
+    const clickHandler = (i) => {
+      setCamIndex(i);  // Trigger re-render
+    }
+
+    const res = Object.entries(rootCam.camGroup).map(([index, cam]) => {
+      return (
+        <Button className={'camera-direction-btn' + ((camera.orientation == cam.orientation) ? ' current' : '') }
+         key={cam.id} onClick={(event) => {event.stopPropagation(); clickHandler(index)}}>
+          {cam.orientation}
+        </Button>
+      );
+    });
+
+    return res;
+  }
+
   return (
     <div className="popup popup--camera">
-      <div onClick={handlePopupClick}>
-        <div className="popup__title">
-          <p className="bold name">{camData.name}</p>
-          <p className="bold orientation">{camData.orientation}</p>
-        </div>
+      {camera &&
+        <div onClick={handlePopupClick}>
+          <div className="popup__title">
+            <p className="bold name">{camera.name}</p>
+            <p className="bold orientation">{camera.orientation}</p>
+          </div>
 
-        <div className="popup__description">
-          <p>{parse(camData.caption)}</p>
-          <div className="popup__camera-info">
-            <div className="camera-orientations">
-              <img className="colocated-camera-icon" src={colocatedCamIcon} role="presentation" alt="colocated cameras icon" />
-              {renderCamGroup(camFeature, setClickedCamera, camData)}
-            </div>
-            <div className="camera-image">
-              <img src={camData.links.imageSource} width='300' />
+          <div className="popup__description">
+            <p>{parse(camera.caption)}</p>
+            <div className="popup__camera-info">
+              <div className="camera-orientations">
+                <img className="colocated-camera-icon" src={colocatedCamIcon} role="presentation" alt="colocated cameras icon" />
+                {renderCamGroup()}
+              </div>
+              <div className="camera-image">
+                <img src={camera.links.imageSource} width='300' />
 
-              <div className="timestamp">
-                <p className="driveBC">Drive<span>BC</span></p>
-                <FriendlyTime date={camData.last_update_modified} />
+                <div className="timestamp">
+                  <p className="driveBC">Drive<span>BC</span></p>
+                  <FriendlyTime date={camera.last_update_modified} />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      }
     </div>
   );
 }
