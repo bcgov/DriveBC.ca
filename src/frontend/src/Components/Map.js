351687a10411dfb87d12b055e293d54230fccca7
bcgov-brwang <87880048+bcgov-brwang@users.noreply.github.com>
2024-01-30 11:20:34 -0800

    DBC22-1617: call toggleMyLocation when first load to center the map to my location if allowed
    DBC22-1617: call toggleMyLocation when first load to center the map to my location if allowed

--- src/frontend/src/Components/Map.js
+++ src/frontend/src/Components/Map.js
@@ -396,6 +396,7 @@ export default function MapWrapper({
     });
 
     loadData(true);
+    toggleMyLocation();
   });
 
   useEffect(() => {
@@ -610,10 +611,15 @@ export default function MapWrapper({
           }
         },
         error => {
-          console.error('Error getting user location:', error);
+          if (error.code === error.PERMISSION_DENIED) {
+            // The user has blocked location access
+            console.error("Location access denied by user.", error);
+          }
+          else {
+            // Zoom out and center to BC if location not available
+            setZoomPan(mapView, 9, fromLonLat([-126.5, 54.2]));
+          }
 
-          // Zoom out and center to BC if location not available
-          setZoomPan(mapView, 9, fromLonLat([-126.5, 54.2]));
         },
       );
     }
