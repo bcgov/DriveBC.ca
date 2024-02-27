5fcf0c3bc993697f2de9bf9afcd41b1c18ac00b7
minORC <min@openroad.ca>
2024-02-27 14:11:34 -0800

    DBC22-1827: Updated icons for minor and major delays, and road conditions (#304)

--- src/frontend/src/Components/EventTypeIcon.js
+++ src/frontend/src/Components/EventTypeIcon.js
@@ -15,26 +15,26 @@ import closuresActiveIcon from '../images/mapIcons/closure-active.png';
 import closuresStaticIcon from '../images/mapIcons/closure-static.png';
 
 // Future Events
-import futureEventsMajorActiveIcon from '../images/mapIcons/futureevent-major-active.png';
-import futureEventsMajorStaticIcon from '../images/mapIcons/futureevent-major-static.png';
-import futureEventsActiveIcon from '../images/mapIcons/futureevent-minor-active.png';
-import futureEventsStaticIcon from '../images/mapIcons/futureevent-minor-static.png';
+import futureEventsMajorActiveIcon from '../images/mapIcons/future-event-major-active.png';
+import futureEventsMajorStaticIcon from '../images/mapIcons/future-event-major-static.png';
+import futureEventsActiveIcon from '../images/mapIcons/future-event-minor-active.png';
+import futureEventsStaticIcon from '../images/mapIcons/future-event-minor-static.png';
 
 // Road Conditions
 import roadConditionsActiveIcon from '../images/mapIcons/road-condition-active.png';
 import roadConditionsStaticIcon from '../images/mapIcons/road-condition-static.png';
 
 // Constructions
-import constructionsMajorActiveIcon from '../images/mapIcons/construction-major-active.png';
-import constructionsMajorStaticIcon from '../images/mapIcons/construction-major-static.png';
-import constructionsActiveIcon from '../images/mapIcons/construction-minor-active.png';
-import constructionsStaticIcon from '../images/mapIcons/construction-minor-static.png';
+import constructionsMajorActiveIcon from '../images/mapIcons/delay-major-active.png';
+import constructionsMajorStaticIcon from '../images/mapIcons/delay-major-static.png';
+import constructionsActiveIcon from '../images/mapIcons/delay-minor-active.png';
+import constructionsStaticIcon from '../images/mapIcons/delay-minor-static.png';
 
 // Generic Events
-import genericDelaysMajorActiveIcon from '../images/mapIcons/generic-event-major-active.png';
-import genericDelaysMajorStaticIcon from '../images/mapIcons/generic-event-major-static.png';
-import genericDelaysActiveIcon from '../images/mapIcons/generic-event-minor-active.png';
-import genericDelaysStaticIcon from '../images/mapIcons/generic-event-minor-static.png';
+import genericDelaysMajorActiveIcon from '../images/mapIcons/incident-major-active.png';
+import genericDelaysMajorStaticIcon from '../images/mapIcons/incident-major-static.png';
+import genericDelaysActiveIcon from '../images/mapIcons/incident-minor-active.png';
+import genericDelaysStaticIcon from '../images/mapIcons/incident-minor-static.png';
 
 export default function EventTypeIcon(props) {
   const { event, state } = props;
