7cfc9eb771330f9d05e1e3988385c70e04300c70
minORC <min@openroad.ca>
2024-03-01 12:35:49 -0800

    DBC22-1732: Added styling and html structure for road weather sidepanel

--- src/frontend/src/Components/map/mapPopup.js
+++ src/frontend/src/Components/map/mapPopup.js
@@ -6,7 +6,14 @@ import EventTypeIcon from '../EventTypeIcon';
 import FriendlyTime from '../FriendlyTime';
 import parse from 'html-react-parser';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFerry } from '@fortawesome/free-solid-svg-icons';
+import { 
+  faFerry,
+  faTemperatureHalf,
+  faMountain,
+  faDroplet,
+  faSnowflake,
+  faWind
+} from '@fortawesome/free-solid-svg-icons';
 
 import './mapPopup.scss';
 
@@ -56,7 +63,6 @@ export function getEventPopup(eventFeature) {
         <div className="popup__title__icon">
           <EventTypeIcon event={eventData} state='active' />
         </div>
-        {console.log(eventData)}
         <p className="name">{convertCategory(eventData)}</p>
       </div>
       <div className="popup__content">
@@ -100,5 +106,77 @@ export function getFerryPopup(ferryFeature) {
         </div>
       </div>
     </div>
+
+    // <div className="popup popup--road-weather">
+    //   <div className="popup__title">
+    //     <div className="popup__title__icon">
+    //       <FontAwesomeIcon icon={faTemperatureHalf} />
+    //     </div>
+    //     <p className="name">Local Weather</p>
+    //     <span className="sub-name">Weather Stations</span>
+    //   </div>
+    //   <div className="popup__content">
+    //     <div className="popup__content__title">
+    //       <p className="name">Trout Creek</p>
+    //       <FriendlyTime date='2024-03-01T00:00:00-08:00' />
+    //       <p className="description">Southside of Highway 15, 22km northwest of Smithers.</p>
+    //     </div>
+    //     <div className="popup__content__description">
+    //       <div className="road-condition">
+    //         <p className="data">Packed snow of 5cm</p>
+    //         <p className="label">Road Condition</p>
+    //       </div>
+    //       <div className="temperatures">
+    //         <div className="temperature temperature--air">
+    //           <p className="data">17&#x2103;</p>
+    //           <p className="label">Air</p>
+    //         </div>
+    //         <div className="temperature temperature--road">
+    //           <p className="data">22&#x2103;</p>
+    //           <p className="label">Road</p>
+    //         </div>
+    //       </div>
+    //       <div className="data-card">
+    //         <div className="data-card__row">
+    //           <div className="data-icon">
+    //             <FontAwesomeIcon className="icon" icon={faMountain} />
+    //           </div>
+    //           <p className="label">Elevation</p>
+    //           <p className="data">410m</p>
+    //         </div>
+    //         <div className="data-card__row">
+    //           <div className="data-icon">
+    //             <FontAwesomeIcon className="icon" icon={faDroplet} />
+    //           </div>
+    //           <p className="label">Precipitation (last 12 hours)</p>
+    //           <p className="data">240mm</p>
+    //         </div>
+    //         <div className="data-card__row">
+    //           <div className="data-icon">
+    //             <FontAwesomeIcon className="icon" icon={faSnowflake} />
+    //           </div>
+    //           <p className="label">Snow (last 12 hours)</p>
+    //           <p className="data">240cm</p>
+    //         </div>
+    //         <div className="data-card__row data-card__row group">
+    //           <div className="data-icon">
+    //             <FontAwesomeIcon className="icon" icon={faWind} />
+    //           </div>
+    //           <div className="data-group">
+    //             <div className="data-group__row">
+    //               <p className="label">Average wind</p>
+    //               <p className="data">SE 15 km/h</p>
+    //             </div>
+    //             <div className="data-group__row">
+    //               <p className="label">Maximum wind</p>
+    //               <p className="data">20 km/h</p>
+    //             </div>
+    //           </div>
+    //         </div>
+
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
