151e43a9b1440abf2c06e3034694d772928289df
minORC <min@openroad.ca>
2024-05-15 14:17:37 -0700

    DBC22-2077: Added more descriptive aria label to beta survey link and container (#451)

--- src/frontend/src/Components/shared/ExitSurvey.js
+++ src/frontend/src/Components/shared/ExitSurvey.js
@@ -22,7 +22,7 @@ export default function ExitSurvey({ mobile=false }) {
     <div className={'exit-survey' + (mobile ? ' mobile' : '')}>
       <Button
         className={'exit-survey-close-btn' + (show ? ' show' : '')}
-        aria-label="close exit survey banner"
+        aria-label="close banner for DriveBC beta feedback survey"
         onClick={() => {
           sessionStorage.setItem('exit-survey', true);
           show ? setShow(false) : setShow(true) }
@@ -39,7 +39,7 @@ export default function ExitSurvey({ mobile=false }) {
         <div className="content__text">
           <p className="bold blue-text">Thank you for checking out the new DriveBC beta!</p>
           <p className="feedback"><span>We&apos;d love to hear your feedback on your experience using the new site. Please take a few minutes to </span>
-          <a className="survey-link" target="_blank" rel="noreferrer" alt="Survey" href="https://forms.office.com/Pages/ResponsePage.aspx?id=AFLbbw09ikqwNtNoXjWa3G-k6A-ZOZVMlxBJti4jf_VURjI4MlRKMlRYQTVFUFJZOU5XTVVZUjEwQS4u" >
+          <a className="survey-link" target="_blank" rel="noreferrer" aria-label="complete our anonymous survey and give feedback on the new DriveBC beta" href="https://forms.office.com/Pages/ResponsePage.aspx?id=AFLbbw09ikqwNtNoXjWa3G-k6A-ZOZVMlxBJti4jf_VURjI4MlRKMlRYQTVFUFJZOU5XTVVZUjEwQS4u" >
           complete our anonymous survey
           </a>
           </p>
