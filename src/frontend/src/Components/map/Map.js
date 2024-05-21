c450f5d6895d9fece3e7c9c02a0dfe19c55b7fa4
ray <ray@oxd.com>
2024-05-21 16:51:04 -0700

    DBC22-1925: route not found display, fix for map fitting

--- src/frontend/src/Components/map/Map.js
+++ src/frontend/src/Components/map/Map.js
@@ -29,6 +29,7 @@ import { addCameraGroups } from '../data/webcams.js';
 import {
   calculateCenter,
   blueLocationMarkup,
+  fitMap,
   onMoveEnd,
   setLocationPin,
   setZoomPan,
@@ -45,6 +46,7 @@ import AdvisoriesOnMap from '../advisories/AdvisoriesOnMap';
 import CurrentCameraIcon from '../cameras/CurrentCameraIcon';
 import ExitSurvey from '../shared/ExitSurvey.js';
 import Filters from '../shared/Filters.js';
+import NoRouteFound from '../routing/NoRouteFound';
 import RouteSearch from '../routing/RouteSearch.js';
 import NetworkErrorPopup from './errors/NetworkError';
 import ServerErrorPopup from './errors/ServerError';
@@ -291,6 +293,10 @@ export default function DriveBCMap(props) {
       mapLayers, mapRef, mapContext,
       'routeLayer', dl, 3
     );
+
+    if (selectedRoute && selectedRoute.routeFound) {
+      fitMap(selectedRoute.route, mapView);
+    }
   }, [selectedRoute]);
 
   // Cameras layer
@@ -404,6 +410,7 @@ export default function DriveBCMap(props) {
               <ExitSurvey mobile={true} />
             )}
             <RouteSearch routeEdit={true} />
+            <NoRouteFound selectedRoute={selectedRoute} />
             <AdvisoriesOnMap advisories={advisoriesInView} updateClickedFeature={updateClickedFeature} open={openPanel} clickedFeature={clickedFeature} clickedFeatureRef={clickedFeatureRef} />
           </div>
         )}
