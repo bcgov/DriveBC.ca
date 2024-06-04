81226c3ffe2e0925b0ca58c8d63c95d7790e66f4
ray <ray@oxd.com>
2024-06-04 06:19:04 -0700

    DBC22-2274: map page implementation

---
+++ src/frontend/src/pages/ReportRoadPage.js
@@ -0,0 +1,29 @@
+// React
+import React from 'react';
+
+// Internal imports
+import { ReportMap } from '../Components/report/ReportMap';
+import Footer from '../Footer';
+import PageHeader from '../PageHeader';
+
+// Styling
+import './ReportRoadPage.scss';
+
+export default function ReportRoadPage() {
+  document.title = `DriveBC - Report Road Maintenance`;
+
+  // Rendering
+  return (
+    <div className='report-road-page'>
+      <PageHeader
+        title="Highway or bridge problem"
+        description="Report highway or bridge problems. Examples include: pot holes, road damage, bridge damage, signage damage, drainage issues, fallen trees, fallen rocks, or animal carcasses."
+        description2="Select the area of the province where you have encountered the highway or bridge problem. If your location is known, it is selected and shown on the map.">
+      </PageHeader>
+
+      <ReportMap />
+
+      <Footer />
+    </div>
+  );
+}
