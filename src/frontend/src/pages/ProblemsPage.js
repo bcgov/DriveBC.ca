a8d09668b8274d8a2d12639866f3057d756db1f6
minORC <min@openroad.ca>
2024-06-11 13:16:00 -0700

    DBC22-2278: Styling Report problem landing page

--- src/frontend/src/pages/ProblemsPage.js
+++ src/frontend/src/pages/ProblemsPage.js
@@ -1,57 +1,101 @@
+// React
 import React from 'react';
-import './ProblemsPage.scss';
+import { Link } from 'react-router-dom';
+
+// External imports
 import Container from 'react-bootstrap/Container';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBoxBallot,
+  faBridge,
+  faPlug,
+  faUtilityPole,
+  faFlask
+} from '@fortawesome/pro-regular-svg-icons';
+
+// Internal imports
 import Footer from '../Footer';
 import PageHeader from '../PageHeader';
-import { Link } from 'react-router-dom';
+
+
+// Styling
+import './ProblemsPage.scss';
 
 const ProblemsPage = () => {
   return (
-    <div className="report-problem">
+    <div className="report-problem-page">
       <PageHeader title='Report a problem'>
       </PageHeader>
       
-      <Container className='text-container'>
+      <Container>
         <div className="report-problem">
-          <div className="problem-categories">
-            <Link to="/website-problem" className="category">
-              <div className="category-icon"></div>
-              <h2>Website problem or suggestion</h2>
-              <p>
-                Report site feedback, suggestions, or bugs related to the new DriveBC website.
-                <br />
-                <i>This feature is currently not available in Beta. Use the Feedback link in the website header to provide feedback.</i>
-              </p>
-            </Link>
-            <Link to="/highway-problem" className="category">
-              <div className="category-icon"></div>
-              <h2>Highway or bridge problem</h2>
-              <p>
-                Report highway or bridge problems. Examples include: pot holes, road damage, bridge damage, signage damage,
-                drainage issues, fallen trees, fallen rocks, or animal carcasses.
-              </p>
-            </Link>
-            <Link to="/road-electrical-problem" className="category">
-              <div className="category-icon"></div>
-              <h2>Road electrical problem</h2>
-              <p>
-                Report an electrical issue on a highway or bridge. Examples include: overhead signs, street lights, pedestrian
-                lighting, signals out, or signals damaged.
-              </p>
-            </Link>
+          <div className="section section--online">
+            <h2>Online</h2>
+            <div className="section__content">
+              <Link to="/website-problem" className="content-card content-card--website disabled">
+                <div className="circle-icon">
+                  <FontAwesomeIcon icon={faBoxBallot} />
+                </div>
+                <h3>Website problem or suggestion</h3>
+                <p>Report website feedback, suggestions, or bugs related to the new DriveBC website.</p>
+                <p className="bold attention">This feature is currently not available in Beta. Alternatively, you may use the Feedback link in the website header.</p>
+              </Link>
+              <Link to="/highway-problem" className="content-card content-card--highway">
+                <div className="circle-icon">
+                  <FontAwesomeIcon icon={faBridge} />
+                </div>
+                <h3>Highway or bridge problem</h3>
+                <p>Report highway or bridge problems. Examples include:</p>
+                <ul>
+                  <li>Pot holes</li>
+                  <li>Road, bridge, and signage damage</li>
+                  <li>Drainage issues</li>
+                  <li>Fallen rocks and trees</li>
+                  <li>Animal carcasses</li>
+                </ul>
+              </Link>
+              <Link to="/road-electrical-problem" className="content-card content-card--electrical">
+                <div className="circle-icon">
+                  <FontAwesomeIcon icon={faPlug} />
+                </div>
+                <h3>Road electrical problem</h3>
+                <p>Report electrical issues on a highway or bridge. Examples include:</p>
+                <ul>
+                  <li>Overhead signs</li>
+                  <li>Street lights</li>
+                  <li>Pedestrian lighting</li>
+                  <li>Signal outages and damages</li>
+                </ul>
+              </Link>
+            </div>
           </div>
-          <div className="emergency-contacts">
-            <div className="contact">
-              <div className="contact-icon"></div>
-              <p><strong>Report a downed power line</strong><br /><span className='underline'>1-888-769-3766</span></p>
-            </div>
-            <div className="contact">
-              <div className="contact-icon"></div>
-              <p><strong>Report a wildfire</strong><br /><span className='underline'>1-800-663-5555</span></p>
-            </div>
-            <div className="contact">
-              <div className="contact-icon"></div>
-              <p><strong>Report a chemical spill</strong><br /><span className='underline'>1-800-663-3456</span></p>
+
+          <div className="section section--phone">
+            <h2>Over the phone</h2>
+            <div className="section__content">
+              <div className="content-card content-card--power">
+                <div className="circle-icon">
+                  <FontAwesomeIcon icon={faUtilityPole} />
+                </div>
+                <p>Report downed power line</p>
+                <a className="tel-number bold" href="tel:1-888-769-3766">1-888-769-3766</a>
+              </div>
+              <div className="content-card content-card--wildfire">
+                <div className="circle-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="38" viewBox="0 0 32 38" fill="none">
+<path d="M0.00134277 22.1706C-0.0708885 16.6567 2.7783 10.8471 7.69802 7.99054L8.13763 8.66611C9.34697 10.1294 10.4467 10.5793 11.986 10.5793C14.5155 10.5793 16.4945 8.55394 16.4945 5.85305V0C16.4945 0 32 8.21488 32 21.755C32 30.5372 25.1091 37.459 17.1648 38V31.9068H23.0979L18.552 24.0957H20.9668L15.9955 16.4495L11.032 24.0957H13.4403L8.90086 31.9068H14.8339V37.9789C6.39717 37.2862 0.104486 30.0873 4.00543e-05 22.1706H0.00134277Z" fill="white"/>
+</svg>
+                </div>
+                <p>Report a wildfire</p>
+                <a className="tel-number bold" href="tel:1-800-663-5555">1-800-663-5555</a>
+              </div>
+              <div className="content-card content-card--chemical">
+                <div className="circle-icon">
+                  <FontAwesomeIcon icon={faFlask} />
+                </div>
+                <p>Report a chemical spill</p>
+                <a className="tel-number bold" href="tel:1-800-663-3456">1-800-663-3456</a>
+              </div>
             </div>
           </div>
         </div>
