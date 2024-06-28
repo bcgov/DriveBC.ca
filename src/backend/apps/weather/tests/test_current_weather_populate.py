31f85ba322a944c4ff585079a497429ab8f45c59
Tyrel Narciso <tyrel@oxd.com>
2024-06-28 09:45:21 -0700

    DBC22-2120: removed logging

--- src/backend/apps/weather/tests/test_current_weather_populate.py
+++ src/backend/apps/weather/tests/test_current_weather_populate.py
@@ -6,9 +6,8 @@ from unittest.mock import patch
 from apps.feed.client import FeedClient
 from apps.shared.tests import BaseTest, MockResponse
 from apps.weather.models import CurrentWeather
-from apps.weather.tasks import (
-    populate_current_weather_from_data,
-)
+from apps.weather.tasks import populate_current_weather_from_data
+
 
 class TestCurrentWeatherPopulate(BaseTest):
     def setUp(self):
@@ -26,8 +25,5 @@ class TestCurrentWeatherPopulate(BaseTest):
     def test_populate_current_weather_function(self):
         populate_current_weather_from_data(self.mock_current_weather_feed_result)
         current_weather_two = CurrentWeather.objects.get(id=298)
-        print(current_weather_two.location_latitude)
         assert current_weather_two.location_latitude == \
                49.16609
-
-
