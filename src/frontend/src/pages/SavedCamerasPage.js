f46ced5008458468545c1b7aa2c44934199b26e9
Min Ji Choi <min@oxd.com>
2024-07-22 14:40:08 -0700

    DDBC22-2378: Style updates for camera cards

--- src/frontend/src/pages/SavedCamerasPage.js
+++ src/frontend/src/pages/SavedCamerasPage.js
@@ -7,7 +7,6 @@ import { useSelector } from 'react-redux';
 import { memoize } from 'proxy-memoize';
 
 // External imports
-import { AsyncTypeahead } from 'react-bootstrap-typeahead';
 import Container from 'react-bootstrap/Container';
 
 // Internal imports
@@ -19,7 +18,6 @@ import ServerErrorPopup from '../Components//map/errors/ServerError';
 import CameraList from '../Components/cameras/CameraList';
 import Footer from '../Footer';
 import PageHeader from '../PageHeader';
-import trackEvent from '../Components/shared/TrackEvent.js';
 
 // Styling
 import './SavedCamerasPage.scss';
@@ -41,9 +39,7 @@ export default function SavedCamerasPage() {
   const isInitialMount = useRef(true);
 
   // UseState hooks
-  const [displayedCameras, setDisplayedCameras] = useState(null);
   const [processedCameras, setProcessedCameras] = useState(null);
-  const [searchText, setSearchText] = useState('');
   const [showNetworkError, setShowNetworkError] = useState(false);
   const [showServerError, setShowServerError] = useState(false);
 
@@ -115,49 +111,6 @@ export default function SavedCamerasPage() {
     getSavedCameras();
   }, [favCams]);
 
-  useEffect(() => {
-    // Search name and caption of all cams in group
-    const searchFn = (pc, targetText) => {
-      const targetLower = targetText.toLowerCase();
-
-      // Sort cameras by the presence of the search text in their name
-      const sortedCamerasByName = pc.camGroup.sort((a, b) => {
-        const aNameMatches = a.name.toLowerCase().includes(targetLower);
-        const bNameMatches = b.name.toLowerCase().includes(targetLower);
-        // Give higher priority to cameras where the name matches the search text
-        return bNameMatches - aNameMatches;
-      });
-
-      for (let i = 0; i < sortedCamerasByName.length; i++) {
-        if (sortedCamerasByName[i].name.toLowerCase().includes(targetLower)) {
-          return true;
-        }
-      }
-
-      // Sort cameras by the presence of the search text in their description
-      const sortedCamerasByDescription = pc.camGroup.sort((a, b) => {
-        const aCaptionMatches = a.caption.toLowerCase().includes(targetLower);
-        const bCaptionMatches = b.caption.toLowerCase().includes(targetLower);
-        // Give higher priority to cameras where the description matches the search text
-        return bCaptionMatches - aCaptionMatches;
-      });
-
-      for (let i = 0; i < sortedCamerasByDescription.length; i++) {
-      if (sortedCamerasByDescription[i].caption.toLowerCase().includes(targetLower)) {
-          return true;
-        }
-      }
-
-      return false;
-    }
-
-    const filteredCams = !searchText ? processedCameras :
-      processedCameras.filter((pc) => searchFn(pc, searchText));
-
-    setDisplayedCameras(filteredCams);
-
-  }, [searchText, processedCameras]);
-
   // Rendering
   return (
     <div className="saved-cameras-page">
@@ -174,35 +127,18 @@ export default function SavedCamerasPage() {
         description="Manage and view your saved cameras here.">
       </PageHeader>
 
-      <Container className="outer-container">
-        <div className="controls-container">
-          <div className="search-container">
-            <AsyncTypeahead
-              id="camera-name-search"
-              isLoading={false}
-              onSearch={() => {}}
-              onBlur={() => {
-                trackEvent('cameras', 'camera-list', 'search', searchText)}}
-              onInputChange={(text) => setSearchText(text)}
-              placeholder={"Find by camera name"}
-              inputProps={{
-                'aria-label': 'input field for camera name search',
-              }}
-            />
+      <Container>
+        <CameraList cameras={ processedCameras ? processedCameras : [] }></CameraList>
+
+        {!(processedCameras && processedCameras.length) &&
+          <div className="empty-cam-display">
+            <h2>No cameras to display</h2>
+
+            <h6><b>Have you entered search terms to narrow down the list?</b></h6>
+            <p>Try checking your spelling, changing, or removing your search terms.</p>
           </div>
-        </div>
+        }
       </Container>
-
-      <CameraList cameras={ displayedCameras ? displayedCameras : [] }></CameraList>
-
-      {!(displayedCameras && displayedCameras.length) &&
-        <Container className="empty-cam-display">
-          <h2>No cameras to display</h2>
-
-          <h6><b>Have you entered search terms to narrow down the list?</b></h6>
-          <p>Try checking your spelling, changing, or removing your search terms.</p>
-        </Container>
-      }
 
       <Footer />
     </div>
