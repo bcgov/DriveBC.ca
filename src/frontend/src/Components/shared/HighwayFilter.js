a90232c2d56ddf1addebfbef0af9edf2bc76d0fe
ray <ray@oxd.com>
2024-08-07 19:21:37 -0700

    DBC22-2562: fixes and styling

--- src/frontend/src/Components/shared/HighwayFilter.js
+++ src/frontend/src/Components/shared/HighwayFilter.js
@@ -1,22 +1,29 @@
 // React
-import React, { useCallback, useState, useEffect } from 'react';
+import React, { useCallback, useRef, useState, useEffect } from 'react';
 
-// Third party packages
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFilter, faXmark } from '@fortawesome/pro-solid-svg-icons';
-import Button from 'react-bootstrap/Button';
-import { AsyncTypeahead } from 'react-bootstrap-typeahead';
-import { updateHighwayFilter } from '../../slices/highwayFilterSlice';
 // Redux
 import { useSelector, useDispatch } from 'react-redux';
 import { memoize } from 'proxy-memoize';
+
+// External imports
+import { AsyncTypeahead } from 'react-bootstrap-typeahead';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilter, faMagnifyingGlass } from '@fortawesome/pro-solid-svg-icons';
+import { faFilter as faFilterOutline } from '@fortawesome/pro-regular-svg-icons';
+import { updateHighwayFilter } from '../../slices/highwayFilterSlice';
+import { useMediaQuery } from '@uidotdev/usehooks';
+
+// Internal imports
 import { collator } from '../data/webcams';
 
 // Styling
 import './HighwayFilter.scss';
 
 export default function HighwayFilters(props) {
+  /* Setup */
   const { getCheckedHighway } = props;
+
+  // Redux
   const dispatch = useDispatch();
   const { highwayFilter } = useSelector(
     useCallback(
@@ -33,6 +40,55 @@ export default function HighwayFilters(props) {
   const [selectedHighway, setSelectedHighway] = useState();
   const [searchText, setSearchText] = useState('');
 
+  // Refs
+  const initialMount = useRef(true);
+
+  // Effects
+  useEffect(() => {
+    // run the collator compare on object.keys, sort the list then set state for orderedHighways
+    const highwayList = searchedHighways ? searchedHighways : Object.keys(highwayFilter);
+
+    const orderedList = highwayList.sort(function (a, b) {
+      return collator.compare(a, b);
+    });
+
+    setOrderedHighways(orderedList);
+  }, [highwayFilter, searchedHighways]);
+
+  useEffect(() => {
+    if (initialMount.current) {
+      initialMount.current = false;
+      return;
+    }
+
+    // reset the searched highways when the search text is empty
+    if (!searchText) {
+      setSearchedHighways(null);
+      return;
+    }
+
+    // search for highway name from text input
+    const searchFn = (highway, targetText) => {
+      const targetLower = targetText.toLowerCase();
+      return highway['highwayName'].toLowerCase().includes(targetLower);
+    };
+
+    const searchableList = Object.values({ ...highwayFilter });
+    const filteredHighways = !searchText ? null : searchableList.filter(highway => searchFn(highway, searchText));
+
+    if (filteredHighways) {
+      setSearchedHighways(filteredHighways.map(highway => highway.id));
+    }
+  }, [searchText]);
+
+  // Reset search text when the filter is closed
+  useEffect(() => {
+    if (!open) {
+      setSearchText('');
+    }
+  }, [open]);
+
+  // Handlers
   const filterHandler = highwayKey => {
     const highways = { ...highwayFilter };
     highways[highwayKey].checked = !highways[highwayKey].checked;
@@ -47,93 +103,71 @@ export default function HighwayFilters(props) {
   const handleRemoveFilter = () => {
     const highways = { ...highwayFilter };
     highways[selectedHighway].checked = false;
+
     dispatch(updateHighwayFilter(highways));
     setSelectedHighway(null);
   };
 
-  useEffect(() => {
-    // run the collator compare on object.keys, sort the list then set state for orderedHighways
-    const highwayList = searchedHighways
-      ? searchedHighways
-      : Object.keys(highwayFilter);
-    const orderedList = highwayList.sort(function (a, b) {
-      return collator.compare(a, b);
-    });
-    setOrderedHighways(orderedList);
-  }, [highwayFilter, searchedHighways]);
+  /* Rendering */
+  // Constants
+  const largeScreen = useMediaQuery('only screen and (min-width : 1200px)');
 
-  useEffect(() => {
-    // search for highway name from text input
-    const searchFn = (highway, targetText) => {
-      const targetLower = targetText.toLowerCase();
-      if (highway['highwayName'].toLowerCase().includes(targetLower)) {
-        return true;
-      }
-      return false;
-    };
-    const searchableList = Object.values({ ...highwayFilter });
-    const filteredHighways = !searchText
-      ? null
-      : searchableList.filter(highway => searchFn(highway, searchText));
-    if (filteredHighways) {
-      setSearchedHighways(filteredHighways.map(highway => highway.id));
-    }
-  }, [searchText]);
-
+  // Main component
   return (
     <div className="filters-component">
-      <Button
-        variant="primary"
-        className={'map-btn open-filters' + (open ? ' open' : '')}
+      <button
+        className={'highway-filter-btn' + (open ? ' open' : '')}
         aria-label="open filters options"
-        onClick={() => {
-          open ? setOpen(false) : setOpen(true);
-        }}>
-        Filter by Highway
-        <FontAwesomeIcon icon={faFilter} />
-      </Button>
-      {open && (
-        <div className="filters">
-          <h4 className="filters-title">Filters</h4>
+        onClick={() => {open ? setOpen(false) : setOpen(true)}}>
 
-          <button
-            className="close-filters"
-            aria-label="close filters options"
-            onClick={() => setOpen(false)}>
-            <FontAwesomeIcon icon={faXmark} />
-          </button>
-          {!selectedHighway && <div>no highways selected</div>}
-          {selectedHighway && (
-            <div>
-              <div>
-                {getCheckedHighway() &&
-                  highwayFilter[getCheckedHighway()].highwayName}
-              </div>
-              <button onClick={() => handleRemoveFilter()}>
-                Remove Filter
-              </button>
-            </div>
-          )}
+        {largeScreen && <div className="caption">Filter by Highway</div>}
+
+        <FontAwesomeIcon icon={selectedHighway ? faFilter : faFilterOutline} />
+
+        {selectedHighway &&
+          <div className="active-count">1</div>
+        }
+      </button>
+
+      {open && (
+        <div className="highway-filters">
           <div className="search-container">
+            <FontAwesomeIcon className="search-icon" icon={faMagnifyingGlass} />
+
             <AsyncTypeahead
               id="highway-filter-search"
               isLoading={false}
               onSearch={() => {}}
               onInputChange={text => setSearchText(text)}
-              placeholder={'Find by Highway'}
+              placeholder={'Search Highways'}
               inputProps={{
                 'aria-label': 'input field for highway filter search',
-              }}
-            />
+              }} />
           </div>
+
+          {!selectedHighway && <div className="selected-filter-container no-selection">No Filters selected</div>}
+
+          {selectedHighway && (
+            <div className="selected-filter-container space-between-row">
+              <div className="selected-filter">
+                {getCheckedHighway() && highwayFilter[getCheckedHighway()].highwayName}
+              </div>
+
+              <div className="remove-btn" onClick={() => handleRemoveFilter()}>
+                Remove Filter
+              </div>
+            </div>
+          )}
+
           {orderedHighways &&
-            orderedHighways.map(highway => {
-              return (
-                <div key={highway} onClick={() => filterHandler(highway)}>
+            <div className="highway-options">
+              {orderedHighways.map(highway =>
+                <div key={highway} className="highway-row" onClick={() => filterHandler(highway)}>
                   <span>{highwayFilter[highway].highwayName}</span>
                 </div>
-              );
-            })}
+              )}
+            </div>
+          }
         </div>
       )}
     </div>
