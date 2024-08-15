c167e8397c41550a523dc7b96b46e4a6a8b9af09
minORC <min@openroad.ca>
2024-08-15 15:59:43 -0700

    Feature/DBC22-2485: Styling updates to Advisory pages (#596)
    * DBC22-2485: Styling updates to the Advisories list page
    
    * DBC22-2485: Styling updates to the Advisory details page

--- src/frontend/src/Components/advisories/AdvisoriesList.js
+++ src/frontend/src/Components/advisories/AdvisoriesList.js
@@ -16,9 +16,9 @@ import {
 import './AdvisoriesList.scss';
 
 
-export default function Advisories(props) {
+export default function AdvisoriesList(props) {
   // State, props and context
-  const { advisories, showDescription, showTimestamp, showPublished, showArrow } = props;
+  const { advisories, showDescription, showTimestamp, showPublished, showArrow, isAdvisoriesListPage } = props;
 
   function handleClick(advisory) {
     trackEvent('click', 'advisories-list', 'Advisory', advisory.title, advisory.teaser);
@@ -28,32 +28,18 @@ export default function Advisories(props) {
   return (
     <ul className="advisories-list">
       {!!advisories && advisories.map((advisory, index) => {
-        return (
-          <a className="advisory-li unread" key={advisory.id} href={`/advisories/${advisory.id}`} onClick={() => handleClick(advisory)}
-            onKeyDown={(keyEvent) => {
-              if (keyEvent.keyCode == 13) {
-                handleClick(advisory);
-              }
-            }}>
-
-            <div className="advisory-li__content" tabIndex={0}>
-              <div className="advisory-li-title-container">
-                <p className='advisory-li-title'>{advisory.title}</p>
-                {(showTimestamp && showPublished) &&
-                <div className="timestamp-container">
-                  <span className="advisory-li-state">{advisory.first_published_at != advisory.last_published_at ? "Updated" : "Published" }</span>
-                  <FriendlyTime date={advisory.latest_revision_created_at} />
+        if (isAdvisoriesListPage) {
+          return (
+            <div className="advisory-li unread" key={advisory.id}>
+              <div className="advisory-li__content">
+                <div className="advisory-li__content__partition advisory-li-title-container">
+                  <a className="advisory-li-title" href={`/advisories/${advisory.id}`}>{advisory.title}</a>
+                  <div className="timestamp-container">
+                    <span className="advisory-li-state">{advisory.first_published_at != advisory.last_published_at ? "Updated" : "Published" }</span>
+                    <FriendlyTime date={advisory.latest_revision_created_at} />
+                  </div>
                 </div>
-                }
-                {(showTimestamp && !showPublished) &&
-                <div className="timestamp-container">
-                  <FriendlyTime date={advisory.latest_revision_created_at} />
-                </div>
-                }
-              </div>
-
-              {showDescription &&
-                <div className='advisory-li-body-container'>
+                <div className='advisory-li__content__partition advisory-li-body-container'>
                   {advisory.teaser &&
                     <div className='advisory-li-body'>{advisory.teaser}</div>
                   }
@@ -62,23 +48,74 @@ export default function Advisories(props) {
                     <div className='advisory-li-body'>{stripRichText(advisory.body)}</div>
                   }
                 </div>
-              }
-
-              {showTimestamp &&
-              <div className="timestamp-container timestamp-container--mobile">
-                <span className="advisory-li-state">{advisory.first_published_at != advisory.last_published_at ? "Updated" : "Published" }</span>
-                <FriendlyTime date={advisory.latest_revision_created_at} />
+                <div className="advisory-li__content__partition timestamp-container timestamp-container--mobile">
+                  <span className="advisory-li-state">{advisory.first_published_at != advisory.last_published_at ? "Updated" : "Published" }</span>
+                  <FriendlyTime date={advisory.latest_revision_created_at} />
+                </div>
+                <div className="button-container">
+                  <a className="viewDetails-link"
+                    href={`/advisories/${advisory.id}`}
+                    aria-label={`View advisory details for ${advisory.title}`}>
+                    View details
+                    <FontAwesomeIcon icon={faChevronRight} />
+                  </a>
+                </div>
               </div>
-              }
             </div>
+          );
+        }
+        else {
+          return (
+            <a className="advisory-li unread" key={advisory.id} href={`/advisories/${advisory.id}`} onClick={() => handleClick(advisory)}
+              onKeyDown={(keyEvent) => {
+                if (keyEvent.keyCode == 13) {
+                  handleClick(advisory);
+                }
+              }}>
+              <div className="advisory-li__content" tabIndex={0}>
+                <div className="advisory-li-title-container">
+                  <p className='advisory-li-title'>{advisory.title}</p>
+                  {(showTimestamp && showPublished) &&
+                  <div className="timestamp-container">
+                    <span className="advisory-li-state">{advisory.first_published_at != advisory.last_published_at ? "Updated" : "Published" }</span>
+                    <FriendlyTime date={advisory.latest_revision_created_at} />
+                  </div>
+                  }
+                  {(showTimestamp && !showPublished) &&
+                  <div className="timestamp-container">
+                    <FriendlyTime date={advisory.latest_revision_created_at} />
+                  </div>
+                  }
+                </div>
 
-            {showArrow &&
-              <div className="advisory-li__arrow">
-                <FontAwesomeIcon icon={faChevronRight} />
+                {showDescription &&
+                  <div className='advisory-li-body-container'>
+                    {advisory.teaser &&
+                      <div className='advisory-li-body'>{advisory.teaser}</div>
+                    }
+
+                    {!advisory.teaser &&
+                      <div className='advisory-li-body'>{stripRichText(advisory.body)}</div>
+                    }
+                  </div>
+                }
+
+                {showTimestamp &&
+                <div className="timestamp-container timestamp-container--mobile">
+                  <span className="advisory-li-state">{advisory.first_published_at != advisory.last_published_at ? "Updated" : "Published" }</span>
+                  <FriendlyTime date={advisory.latest_revision_created_at} />
+                </div>
+                }
               </div>
-            }
-          </a>
-        );
+
+              {showArrow &&
+                <div className="advisory-li__arrow">
+                  <FontAwesomeIcon icon={faChevronRight} />
+                </div>
+              }
+            </a>
+          );
+        }
       })}
     </ul>
   );
--- src/frontend/src/Components/advisories/EmptyAdvisory.js
+++ src/frontend/src/Components/advisories/EmptyAdvisory.js
@@ -1,11 +1,10 @@
 // React
 import React from 'react';
-import Container from 'react-bootstrap/Container';
 
 export default function EmptyAdvisory() {
   // Rendering
   return (
-    <Container className="empty-advisory-display">
+    <div className="empty-advisory-display">
       <h2>No advisories are active at the moment</h2>
       <p>
         If there are widespread travel advisories, they will be posted here to give you guidance on:
@@ -15,6 +14,6 @@ export default function EmptyAdvisory() {
         <li>where to detour if available, and</li>
         <li>keep you informed with the most up-to-date information we have.</li>
       </ul> 
-    </Container>
+    </div>
   );
 }
