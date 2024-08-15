e402bfc6c9b554bb5f6b1b4b5a86a505579da45b
Min Ji Choi <min@oxd.com>
2024-08-15 15:08:25 -0700

    DBC22-2485: Styling updates to the Advisories list page

--- src/frontend/src/Components/bulletins/EmptyBulletin.js
+++ src/frontend/src/Components/bulletins/EmptyBulletin.js
@@ -1,17 +1,16 @@
 // React
 import React from 'react';
-import Container from 'react-bootstrap/Container';
 
 export default function EmptyBulletin() {
   // Rendering
   return (
-    <Container className="empty-advisory-display">
+    <div className="empty-advisory-display">
       <h2>No bulletins at the moment</h2>
       <p>
         There are currently no bulletins to display. 
         As we continue to evolve DriveBC to better meet the needs of British Columbians, 
         watch this space for updates as they come!
       </p>
-  </Container>
+  </div>
   );
 }
