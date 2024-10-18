88f097a1c0638503eff6d46830b7c385f40b226e
Mulder <Willem.Mulder@gov.bc.ca>
2024-10-18 13:11:55 -0700

    DBC22-2932: Updates to Env Canada Alerts
    -Fixed issue where Env Canada now only returns URL in each event which broke the displaying of events on the site (requires an API fix from the SAWSx team too)
    -Added Env Canada warnings to High Elevation Forecasts
    -Display multiple warnings on two rows instead of side by side, now each with their own details link.
    -Added Courtesy of Environment Canada

--- src/backend/apps/weather/admin.py
+++ src/backend/apps/weather/admin.py
@@ -1,4 +1,4 @@
-from apps.weather.models import CurrentWeather, RegionalWeather
+from apps.weather.models import CurrentWeather, RegionalWeather, HighElevationForecast
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
@@ -6,6 +6,9 @@ from django.contrib.admin import ModelAdmin
 class WeatherAdmin(ModelAdmin):
     readonly_fields = ('id', )
 
+class HefAdmin(ModelAdmin):
+    readonly_fields = ('code', )
 
 admin.site.register(RegionalWeather, WeatherAdmin)
 admin.site.register(CurrentWeather, WeatherAdmin)
+admin.site.register(HighElevationForecast, HefAdmin)
