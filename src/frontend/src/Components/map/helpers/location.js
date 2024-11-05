efbe1ae2018e683476b5e97588e77c08a9bc4309
bcgov-brwang <87880048+bcgov-brwang@users.noreply.github.com>
2024-11-05 12:53:13 -0800

    DBC22-2810: adjusted size of the location pin mark as requested (#706)
    * DBC22-2810: adjusted size of the location pin mark as requested
    
    DBC22-2810: adjusted size of the location pin mark as requested
    
    * DBC22-2810: rework on size of location pin markup
    
    DBC22-2810: rework on size of location pin markup
    
    * DBC22-2810: updated red mark to 28px
    
    DBC22-2810: updated red mark to 28px

--- src/frontend/src/Components/map/helpers/location.js
+++ src/frontend/src/Components/map/helpers/location.js
@@ -3,40 +3,40 @@ import Overlay from 'ol/Overlay.js';
 
 // Location pins
 export const blueLocationMarkup = `
-  <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg" id="svg-container">
+  <svg width="44" height="44" xmlns="http://www.w3.org/2000/svg" id="svg-container">
     <defs>
       <linearGradient id="gradient1" x1="0%" y1="0%" x2="100%" y2="0%">
         <stop offset="40%" style="stop-color:#f32947;stop-opacity:0.5" />
         <stop offset="40%" style="stop-color:#ed6f82;stop-opacity:0.5" />
       </linearGradient>
     </defs>
-    <circle id="circle1" cx="44" cy="44" r="44" fill="url(#gradient1)"/>
+    <circle id="circle1" cx="22" cy="22" r="22" fill="url(#gradient1)"/>
     <defs>
       <linearGradient id="gradient2" x1="0%" y1="0%" x2="100%" y2="0%">
         <stop offset="100%" style="stop-color:#f32947;stop-opacity:1" />
         <stop offset="100%" style="stop-color:#ed6f82;stop-opacity:1" />
       </linearGradient>
     </defs>
-    <circle cx="44" cy="44" r="16" fill="url(#gradient2)" stroke="white" stroke-width="2" />
+    <circle cx="22" cy="22" r="11" fill="url(#gradient2)" stroke="white" stroke-width="2" />
   </svg>
 `;
 
 export const redLocationMarkup = `
-  <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg" id="svg-container">
+  <svg width="28" height="28" xmlns="http://www.w3.org/2000/svg" id="svg-container">
     <defs>
       <linearGradient id="gradient1" x1="0%" y1="0%" x2="100%" y2="0%">
         <stop offset="40%" style="stop-color:#2790F3;stop-opacity:0.5" />
         <stop offset="40%" style="stop-color:#7496EC;stop-opacity:0.5" />
       </linearGradient>
     </defs>
-    <circle id="circle1" cx="44" cy="44" r="44" fill="url(#gradient1)"/>
+    <circle id="circle1" cx="14" cy="14" r="14" fill="url(#gradient1)"/>
     <defs>
       <linearGradient id="gradient2" x1="0%" y1="0%" x2="100%" y2="0%">
         <stop offset="100%" style="stop-color:#2970F3;stop-opacity:1" />
         <stop offset="100%" style="stop-color:#7496EC;stop-opacity:1" />
       </linearGradient>
     </defs>
-    <circle cx="44" cy="44" r="16" fill="url(#gradient2)" stroke="white" stroke-width="2" />
+    <circle cx="14" cy="14" r="7" fill="url(#gradient2)" stroke="white" stroke-width="2" />
   </svg>
 `;
 
@@ -50,6 +50,8 @@ export const redLocationToMarkup = `
     class="svg-inline--fa fa-location-dot" 
     role="img" 
     xmlns="http://www.w3.org/2000/svg" 
+    width="28" 
+    height="28"
     viewBox="0 0 384 512">
     
     <path 
@@ -87,11 +89,4 @@ export const setLocationPin = (coordinates, svgMarkup, mapRef, pinRef) => {
   }
 
   mapRef.current.addOverlay(pinOverlay);
-  mapRef.current.on('moveend', function (event) {
-    const newZoom = mapRef.current.getView().getZoom();
-    // Calculate new marker size based on the zoom level
-    const newSize = 44 * (newZoom / 10);
-    svgImage.style.width = newSize + 'px';
-    svgImage.style.height = newSize + 'px';
-  });
 }
