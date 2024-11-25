1043620eeefe15403ba2636fc6593412f3e2b237
bcgov-brwang <87880048+bcgov-brwang@users.noreply.github.com>
2024-11-25 09:37:21 -0800

    DBC22-2870: deleted rest stop records that not available from the rest stop API
    DBC22-2870: deleted rest stop records that not available from the rest stop API

--- src/backend/apps/rest/tasks.py
+++ src/backend/apps/rest/tasks.py
@@ -32,8 +32,18 @@ def populate_all_rest_stop_data():
     client = FeedClient()
     feed_data = client.get_rest_stop_list()
 
+    # Purge rest stops absent in the feed
+    available_rest_stop_ids = []
     for rest_stop_data in feed_data:
-        populate_rest_stop_from_data(rest_stop_data)
-
+        try:
+            chris_rest_area_id = rest_stop_data.get('properties', {}).get('CHRIS_REST_AREA_ID')
+            populate_rest_stop_from_data(rest_stop_data)
+            if chris_rest_area_id:
+                available_rest_stop_ids.append(chris_rest_area_id)
+        except Exception as e:
+            logger.warning(e)
+    RestStop.objects.exclude(rest_stop_id__in=available_rest_stop_ids)\
+        .delete()
+    
     # Rebuild cache
     cache.delete(CacheKey.REST_STOP_LIST)
