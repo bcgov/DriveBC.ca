d98617c5f17ac2bb0ce5541473c0df98257a5762
ray <ray@oxd.com>
2024-11-26 14:33:21 -0800

    DBC22-2870: removed try/catch

--- src/backend/apps/rest/tasks.py
+++ src/backend/apps/rest/tasks.py
@@ -35,15 +35,13 @@ def populate_all_rest_stop_data():
     # Purge rest stops absent in the feed
     available_rest_stop_ids = []
     for rest_stop_data in feed_data:
-        try:
-            chris_rest_area_id = rest_stop_data.get('properties', {}).get('CHRIS_REST_AREA_ID')
-            populate_rest_stop_from_data(rest_stop_data)
-            if chris_rest_area_id:
-                available_rest_stop_ids.append(chris_rest_area_id)
-        except Exception as e:
-            logger.warning(e)
+        chris_rest_area_id = rest_stop_data.get('properties', {}).get('CHRIS_REST_AREA_ID')
+        populate_rest_stop_from_data(rest_stop_data)
+        if chris_rest_area_id:
+            available_rest_stop_ids.append(chris_rest_area_id)
+
     RestStop.objects.exclude(rest_stop_id__in=available_rest_stop_ids)\
         .delete()
-    
+
     # Rebuild cache
     cache.delete(CacheKey.REST_STOP_LIST)
