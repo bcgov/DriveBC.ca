575b45e869d0f1703d35362d71ce453bf200d249
ray <ray@oxd.com>
2024-11-27 16:32:04 -0800

    DBC22-2330: added ferry model admin

---
+++ src/backend/apps/ferry/admin.py
@@ -0,0 +1,10 @@
+from apps.event.models import Ferry
+from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
+
+class FerryAdmin(ModelAdmin):
+    readonly_fields = ('id', )
+
+
+admin.site.register(Ferry, FerryAdmin)
