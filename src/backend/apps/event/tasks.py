672157c64a4a5a0b7069b98920feed023b72c162
ray <ray@oxd.com>
2024-12-10 09:56:27 -0800

    DBC22-3081: fixed timezone conversion offset

--- src/backend/apps/event/tasks.py
+++ src/backend/apps/event/tasks.py
@@ -1,5 +1,6 @@
 import datetime
 import logging
+from zoneinfo import ZoneInfo
 
 import pytz
 from apps.event.enums import EVENT_DIFF_FIELDS, EVENT_STATUS, EVENT_UPDATE_FIELDS
@@ -120,7 +121,7 @@ def populate_all_event_data():
 
             # DBC22-3081 replace timezone with DIT API data, to be removed if source is corrected
             if cars_data['timezone']:
-                new_tz = pytz.timezone(cars_data['timezone'])
+                new_tz = ZoneInfo(cars_data['timezone'])
 
                 first_created_time = event_data["first_created"].replace(tzinfo=new_tz)
                 event_data["first_created"] = cap_time_to_now(first_created_time)
@@ -129,11 +130,11 @@ def populate_all_event_data():
                 event_data["last_updated"] = cap_time_to_now(last_updated_time)
 
                 if "start" in event_data:
-                    pacific_start_time = event_data["start"].astimezone(pytz.timezone('America/Vancouver'))
+                    pacific_start_time = event_data["start"].astimezone(ZoneInfo('America/Vancouver'))
                     event_data["start"] = pacific_start_time.replace(tzinfo=new_tz)
 
                 if "end" in event_data:
-                    pacific_end_time = event_data["end"].astimezone(pytz.timezone('America/Vancouver'))
+                    pacific_end_time = event_data["end"].astimezone(ZoneInfo('America/Vancouver'))
                     event_data["end"] = pacific_end_time.replace(tzinfo=new_tz)
 
             # Populate db obj
