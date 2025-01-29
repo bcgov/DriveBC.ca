70076811c88fd54ef341dff89c7d7e15501c39f2
ray <ray@oxd.com>
2025-01-28 17:28:32 -0800

    DBC22-2802: payload integration

--- src/backend/apps/authentication/serializers.py
+++ src/backend/apps/authentication/serializers.py
@@ -11,13 +11,17 @@ class FavouritedCamerasSerializer(serializers.ModelSerializer):
 
 
 class SavedRoutesSerializer(SafeStringMixin, serializers.ModelSerializer):
-
     class Meta:
         model = SavedRoutes
-        fields = ('id', 'label', 'distance', 'distance_unit',
-                  'start', 'start_point', 'end', 'end_point',
-                  'validated', 'thumbnail', 'route', 'criteria',
-                  'searchTimestamp', 'notification')
+        fields = (
+            'id', 'label', 'distance', 'distance_unit',
+            'start', 'start_point', 'end', 'end_point',
+            'validated', 'thumbnail', 'route', 'criteria',
+            'searchTimestamp', 'notification', 'notification_types',
+            'notification_days', 'notification_start_date',
+            'notification_end_date', 'notification_start_time',
+            'notification_end_time',
+        )
 
     def save(self):
         ''' Save the route with the requesting user. '''
