1db6603118066dd5b1cb241315259dc62b69eec5
Mulder <Willem.Mulder@gov.bc.ca>
2025-03-21 11:51:44 -0700

    DBC22-3599: Add Standbycheck

---
+++ compose/standbycheck/app/lbcheck.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+set -o pipefail
+set -o nounset
+
+# secret should provide variables. assignments fail if variables don't exist
+l_namespace=${NAMESPACE}                            # GoldDR namespace
+l_cluster_name=${STANDBY_POSTGRESCLUSTER_NAME}      # name of PostgresCluster object
+l_ocp_api_server=${STANDBY_OCP_API_SERVER}          # GoldDR login server
+l_serviceaccount_token=${SERVICEACCOUNT_TOKEN}      # GoldDR service account token
+
+status="true"                                       # failsave default for standby_status
+failures=0                                          # failure count init
+
+max_failures=3                                      # max failures until we shutdown
+sleep=30                                            # sleep duration between checks
+login_sleep=30                                      # sleep duration between login attempts
+caddy_200_conf="/app/Caddyfile_200"  		        # caddy 200 OK configuration
+caddy_503_conf="/app/Caddyfile_503"                 # caddy 503 Error configuration
+
+echodate() {
+    echo `date +%Y/%m/%d\ %H:%M:%S`:: $*
+}
+
+standby_status() {
+    output=$(oc -n ${l_namespace} get postgrescluster ${l_cluster_name} -o jsonpath="{.spec.standby.enabled}")
+    
+    if [[ $? -eq 0 ]]; then
+        echo $output
+    else
+        echo "unknown"
+    fi
+}
+
+shutdown_status() {
+    output=$(oc -n ${l_namespace} get postgrescluster ${l_cluster_name} -o jsonpath="{.spec.shutdown}")
+    
+    if [[ $? -eq 0 ]]; then
+        echo $output
+    else
+        echo "unknown"
+    fi
+}
+
+caddy_reload_config() {
+    caddy reload -c $1 > /dev/null 2>&1
+    ret=$?
+    if [[ $ret -eq 0 ]]; then
+        echodate "[NOTICE] Reloaded caddy configuration with: $1"
+    else
+        echodate "[CRITICAL] Unable to reload caddy configuration with: $1. Returned error code $ret"
+    fi
+}
+
+while true; do
+    oc login --token=$l_serviceaccount_token --server=${l_ocp_api_server}
+    if [[ $? -eq 0 ]]; then
+        echodate "[NOTICE] Successful login to OpenShift"
+        break
+    else
+        echodate "[CRITICAL] Unable to login to cluster. Check namespace and service accout configuration."
+        echodate "[CRITICAL] Assuming standby cluster is TRUE. Responding with HTTP 200. Retry in ${login_sleep}s."
+        sleep ${login_sleep}
+    fi
+done
+
+standbyspec=$(standby_status)
+echodate "[NOTICE] ${l_namespace}: Starting PostgresCluster standby watch. Current status of ${l_cluster_name}: ${standbyspec^^}"
+
+while true; do
+    standbyspec=$(standby_status)
+    shutdownspec=$(shutdown_status)
+
+    case "$standbyspec" in
+        # standby spec is true - postgrescluster is in standby mode
+        "true")
+            echodate "[NOTICE] ${l_namespace}: Checking standby status for PostgresCluster ${l_cluster_name}: ${standbyspec^^}"
+            failures=0
+			caddy_reload_config ${caddy_200_conf}
+        ;;
+
+        # standby spec is false - postgrescluster is in primary mode and we should shut down
+        "false")
+            if [[ $shutdownspec -eq "true" ]]; then
+                echodate "[CRITICAL]: ${l_namespace}: GOLDDR is in primary mode AND in a shutdown state. Staying up."
+                caddy_reload_config ${caddy_200_conf}
+                continue
+            fi
+            
+            echodate "[CRITICAL] ${l_namespace}: Checking standby status for PostgresCluster ${l_cluster_name}: ${standbyspec^^}"
+            echodate "[CRITICAL] ${l_namespace}: GOLDDR cluster is set to primary. Sending HTTP 503 for GOLD load balancer health check."
+            
+            
+            # fail 3 times before switching
+            failures=$((failures+1))
+            if [[ $failures -lt 3 ]]; then
+                echodate "[CRITCAL] ${l_namespace}: Failure count (${failures}) < max_failure_count (${max_failures})."
+                echodate "[CRITCAL] ${l_namespace}: Not switching yet."
+                sleep ${sleep}
+                continue
+            fi
+
+            # hit max failures. replace caddy config and reload caddy.
+            if [[ $failures -ge $max_failures ]]; then
+                echodate "[CRITCAL] ${l_namespace}: Failure count (${failures}) => max_failure_count (${max_failures})."
+                echodate "[CRITCAL] ${l_namespace}: DISABLING LOAD BALANCER CHECK. CHANGING caddy CONFIG TO RETURN 503."
+                caddy_reload_config ${caddy_503_conf}
+            fi
+        ;;
+
+        # standby spec is undefined - sane default is to stay up and running
+        *)
+            echodate "[WARNING] ${l_namespace}: Standby status for ${l_cluster_name} unknown! Assuming stable and healthy."
+            caddy_reload_config ${caddy_200_conf}
+        ;;
+    esac
+    sleep ${sleep}
+done
---
+++ infrastructure/standbycheck/README.md
@@ -0,0 +1,73 @@
+# Standby Check Tool
+The purpose of this chart is to deploy a simple application that 
+- Gets deployed to both Gold and Gold DR that returns a 200 `server up` for the GSLB to check to confirm the cluster is up
+- The instance on Gold will return a 200 by default, but if
+    - If GoldDR CrunchyDB is Primary then it will return a 503 so that the GSLB doesn't automatically send traffic back to Gold after the DB was failed over
+    - If GoldDR CrunchyDB is Primary and Shutdown it will return a 200 so that the load balancer starts sending traffic back to Gold as we can assume we are failing back to Gold
+
+To accomplish this we set this up:
+- Deployment in both clusters
+    - Gold will do the validation
+    - GoldDR will always return a 200
+- Service and Route (IP restricted) for traffic to reach the `/lbhealth/` path
+- Secret in Gold for users to enter:
+    - `NAMESPACE`
+    - `SERVICEACCOUNT_TOKEN`
+    - `STANDBY_OCP_API_SERVER`
+    - `STANDBY_POSTGRESCLUSTER_NAME`
+- In Gold DR we need a mechanism for the Gold cluster to check the status of the Postgres Cluster object so we setup these additional components:
+    - Service Account
+    - Role
+    - Role Binding
+    - Service Account Secret
+
+To address limitations of the pipeline service account we use for deployments, once the Service Account related components are installed using `helm install`, they will never be updated during a `helm upgrade`.
+
+## Initial Deployment
+You will need to create the appropriate Values file for your environment, setting things like name, etc.
+
+Once you are ready to deploy for the first time to an environment follow these steps using your own account that has admin access to the namespace:
+1. Login to OC for Gold
+1. Navigate to the `infrastructure` folder
+1. Run `helm install ENV-drivebc-standbycheck -f ./standbycheck/values-ENV.yaml ./standbycheck`
+1. Login to OC for GoldDR
+1. Run `helm install ENV-drivebc-standbycheck-dr -f ./standbycheck/values-ENV.yaml -f ./standbycheck/values-ENV-dr.yaml ./standbycheck`
+1. Go to OpenShift Gold and do the following:
+    1. Go to Secrets
+    1. Find `ENV-drivebc-standbycheck`
+    1. Actions -> Edit Secret
+    1. Fill in the 
+        - `NAMESPACE`: One you set this up in. Include the -dev, -test or -prod
+        - `SERVICEACCOUNT_TOKEN`: This token can be found in the GoldDR cluster by going to Secrets, ENV-drivebc-standbycheck, and copying the token field from there.
+        - `STANDBY_OCP_API_SERVER`: The GoldDR API Server which you can see when you get your own token
+        - `STANDBY_POSTGRESCLUSTER_NAME`: The name of the postgres cluster object in Gold DR
+    1. Go to Deployments and find `ENV-drivebc-standbycheck`
+    1. Click the vertical ... and select `Restart Rollout` so that the new secrets get consumed
+    1. Look at the logs of one of the pods to confirm it can connect to GoldDR
+    1. You can check the route on both clusters to confirm it's returning a `server up` message.
+
+## Upgrades
+Occasionally you may need to update the image with minor changes or security patches. Typically this will be hanlded through the Github Action, but if you want to do it locally you can run these commands.
+I suggest you add the sha of the image you want to use since the default image is latest, but since the imagePullPolicy is set to `IfNotPresent` you may not get an old image.
+
+Gold: 
+`helm ugprade ENV-drivebc-standbycheck -f ./standbycheck/values-ENV.yaml ./standbycheck --set image.tag=<ENTER SHA>`
+
+Gold DR:
+`helm upgrade dev-drivebc-standbycheck -f ./standbycheck/values-dev.yaml -f ./standbycheck/values-dev-dr.yaml ./standbycheck --set image.tag=sha-<ENTER SHA>`
+
+## Delete
+Simply follow these steps:
+1. Login to the correct project
+1. `helm list` to confirm your helm release is there
+1. `helm uninstall ENV-drivebc-standbycheck`
+1. On GoldDR also run these
+    ```
+    oc delete rolebinding ENV-drivebc-standbycheck
+    oc delete role ENV-drivebc-standbycheck
+    oc delete serviceaccount ENV-drivebc-standbycheck
+    oc delete secret ENV-drivebc-standbycheck
+    ```
+1. On Gold run this command `oc delete secret ENV-drivebc-standbycheck`
+
+We need to delete those components manually because Github actions is using an OpenShift service account that doesn't have permissions on all those objects so we had to set a few to be generated on initial install but not touched on upgrades.
