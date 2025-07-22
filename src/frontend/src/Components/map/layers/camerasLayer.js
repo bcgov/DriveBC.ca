be85133cbe534c036bd662dafd06e672bdf85c55
ray <ray@oxd.com>
2025-07-22 12:33:28 -0700

    DBC22-4492: fixed cam reference feature equality check

--- src/frontend/src/Components/map/layers/camerasLayer.js
+++ src/frontend/src/Components/map/layers/camerasLayer.js
@@ -34,7 +34,7 @@ export function getCamerasLayer(cameras, projectionCode, mapContext, referenceDa
     // Update the reference feature if one of the cameras is the reference
     if (referenceData?.type === 'camera') {
       feature.get('camGroup').forEach((cam) => {
-        if (cam.id === referenceData.id) {
+        if (cam.id == referenceData.id) {  // Intentional loose equality check
           if (referenceData.focusCamera) {
             feature.set('focusCamera', referenceData.focusCamera);
             feature.set('zoom', referenceData.zoom);
