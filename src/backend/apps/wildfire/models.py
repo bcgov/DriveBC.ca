244eb02e26a09117cc132e257a0bc4c9cad9c071
ray <ray@oxd.com>
2025-09-03 12:00:41 -0700

    DBC22-4719: wildfire names can now be null

--- src/backend/apps/wildfire/models.py
+++ src/backend/apps/wildfire/models.py
@@ -5,7 +5,7 @@ from django.contrib.gis.db import models
 class Wildfire(BaseModel):
     id = models.CharField(max_length=128, primary_key=True)
     url = models.URLField(null=True, blank=True)
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, null=True, blank=True, default='')
 
     geometry = models.GeometryField()
     location = models.PointField()
