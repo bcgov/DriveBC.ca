c0b256abe264ab631ca0c5959daca0ca4c045732
bcgov-brwang <87880048+bcgov-brwang@users.noreply.github.com>
2025-09-04 22:52:58 -0700

    resolve migrate conflicts

--- src/backend/apps/webcam/migrations/0012_webcam_hev_station_webcam_local_weather_station_and_more.py
+++ src/backend/apps/webcam/migrations/0012_webcam_hev_station_webcam_local_weather_station_and_more.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('weather', '0016_currentweather_code'),
-        ('webcam', '0010_webcam_group_webcam_nearby_objs'),
+        ('webcam', '0011_webcam_group_webcam_nearby_objs'),
     ]
 
     operations = [
