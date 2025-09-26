710154518d0d1db7880058bbfd7f148e2392f4a2
Min Ji Choi <min@oxd.com>
2025-09-26 15:14:32 -0700

    DBC22-4857: Added thumbnail image update and update status

--- src/frontend/src/Components/cameras/directions/CameraOrientations.js
+++ src/frontend/src/Components/cameras/directions/CameraOrientations.js
@@ -5,8 +5,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faChevronRight,
-  faChevronLeft,
-  faVideoSlash
+  faChevronLeft
 } from '@fortawesome/pro-solid-svg-icons';
 import { useMediaQuery } from '@uidotdev/usehooks';
 import Button from 'react-bootstrap/Button';
@@ -16,6 +15,7 @@ import GoodCarousel from 'react-good-carousel';
 import { getCameraOrientation, getFullOrientation } from "../helper";
 import colocatedCamIcon from '../../../images/colocated-camera.svg';
 import trackEvent from "../../shared/TrackEvent";
+import CameraThumbnail from './CameraThumbnail';
 
 // Styling
 import './CameraOrientations.scss';
@@ -34,12 +34,11 @@ export default function CameraOrientations(props) {
   const imageRef = useRef(null);
 
   // States
-  const [camera, setCamera] = useState(camData);
-  const showCompactLayout = (camera.camGroup.length > 2) && smallScreen;
-  const showReducedLayout = (camera.camGroup.length > 3) && inBetweenScreen;
+  const showCompactLayout = (camData.camGroup.length > 2) && smallScreen;
+  const showReducedLayout = (camData.camGroup.length > 3) && inBetweenScreen;
   const showFullLayout = (
-    ((camera.camGroup.length < 3) && smallScreen) ||
-    ((camera.camGroup.length < 4) && inBetweenScreen) ||
+    ((camData.camGroup.length < 3) && smallScreen) ||
+    ((camData.camGroup.length < 4) && inBetweenScreen) ||
     xXLargeScreen
   );
   const perPane = showCompactLayout ? 2 : 3;
@@ -47,36 +46,19 @@ export default function CameraOrientations(props) {
   const initialIndex = camData.camGroup.findIndex(cam => cam.id === camData.id);
   const [currentCamIndex, setCurrentCamIndex] = useState(initialIndex);
   const [currentPane, setCurrentPane] = useState(Math.floor(initialIndex / perPane));
-  const [isUpdated, setIsUpdated] = useState(false);
 
   // Effects
   useEffect(() => {
-    const nextCam = camera.camGroup[currentCamIndex];
+    const nextCam = camData.camGroup[currentCamIndex];
     loadCamDetails(nextCam);
   }, [currentCamIndex]);
 
-  useEffect(() => {
-    if (camera.last_update_modified !== camData.last_update_modified) {
-      updateCameraImage(camData);
-    }
-
-    setCamera(camData);
-  }, [camData]);
-
-  /* Helpers */
-  const updateCameraImage = (cam) => {
-    // Update the image src
-    if (imageRef.current) {
-      imageRef.current.src = `${cam.links.imageDisplay}?ts=${new Date(cam.last_update_modified).getTime()}`;
-    }
-  }
-
   /* Handlers */
   const rotateCameraOrientation = () => {
-    const currentIndex = camera.camGroup.findIndex(cam => cam.id === camera.id);
-    const nextIndex = (currentIndex + 1) % camera.camGroup.length;
+    const currentIndex = camData.camGroup.findIndex(cam => cam.id === camData.id);
+    const nextIndex = (currentIndex + 1) % camData.camGroup.length;
 
-    const nextCamera = camera.camGroup[nextIndex];
+    const nextCamera = camData.camGroup[nextIndex];
     loadCamDetails(nextCamera);
     trackEvent("click", "camera-details", "camera-rotate", nextCamera.name);
   };
@@ -85,35 +67,6 @@ export default function CameraOrientations(props) {
   const switchOrientation = (index) => {
     setCurrentCamIndex(index);
     setCurrentPane(Math.floor(index / perPane));
-  }
-
-  const stale = camera && camera.marked_stale ? 'stale' : '';
-  const delayed = camera && camera.marked_delayed ? 'delayed' : '';
-  const unavailable = camera && camera.is_on ? '' : 'unavailable';
-  const updated = isUpdated ? 'updated' : '';
-
-  // get camera state message
-  const getStateMessage = () => {
-    if (updated) {
-      return 'Updated';
-    }
-
-    else {
-      if (unavailable) {
-        return 'Unavailable';
-      }
-      else {
-        if (delayed && stale) {
-          return 'Delayed';
-        }
-        else if (!delayed && stale) {
-          return 'Stale';
-        }
-        else {
-          return '';
-        }
-      }
-    }
   }
 
   /* Rendering */
@@ -145,44 +98,12 @@ export default function CameraOrientations(props) {
             itemPeek={12}
             animationDuration={0.4}>
 
-            {camera.camGroup.map((cam, index) => (
-              <div
-                key={cam.id}
-                className={'orientation' + (camera.id === cam.id ? ' current' : '')}
-                onClick={() => {
-                  switchOrientation(index);
-                  trackEvent("click", "camera-details", "mobile-camera-orientation", cam.name);
-                }}
-                onKeyDown={keyEvent => {
-                  if (['Enter', 'NumpadEnter'].includes(keyEvent.key)) {
-                    switchOrientation(index);
-                    trackEvent("click", "camera-details", "mobile-camera-orientation", cam.name);
-                  }
-                }}>
-
-                <img
-                  ref={imageRef}
-                  src={cam.links.imageDisplay}
-                  alt={cam.name}/>
-
-                {(!stale && !delayed && !unavailable && !updated) &&
-                <span
-                  className={'status-pill' + (stale ? ' stale' : '') + (delayed ? ' delayed' : '') + (unavailable ? ' unavailable' : '') + (updated ? ' updated' : '')}>
-                    {getStateMessage()}
-                </span>
-}
-
-                <span
-                  aria-label={getCameraOrientation(cam.orientation)}
-                  className={'label' + (camera.id === cam.id ? ' current' : '')}>
-
-                  {getFullOrientation(cam.orientation)}
-                </span>
-              </div>
+            {camData.camGroup.map((cam, index) => (
+              <CameraThumbnail key={cam.id} thumbnailCamera={cam} mainCamera={camData.camGroup[currentCamIndex]} index={index} switchOrientation={switchOrientation} />
             ))}
           </GoodCarousel>
 
-          {currentCamIndex < (camera.camGroup.length - 1) && (
+          {currentCamIndex < (camData.camGroup.length - 1) && (
             <Button
               className="carousel-button next"
               onClick={() => switchOrientation(currentCamIndex + 1)}>
@@ -201,47 +122,9 @@ export default function CameraOrientations(props) {
       }
 
       {showFullLayout &&
-        <div className={'main-content' + ' camCount' + camera.camGroup.length}>
-          {camera.camGroup.map((cam, index) => (
-            <div
-              key={cam.id}
-              className={'orientation' + (camera.id === cam.id ? ' current' : '') + (unavailable ? ' unavailable' : '')}
-              onClick={() => {
-                switchOrientation(index);
-                trackEvent("click", "camera-details", "camera-orientation", cam.name);
-              }}
-              onKeyDown={keyEvent => {
-                if (['Enter', 'NumpadEnter'].includes(keyEvent.key)) {
-                  switchOrientation(index);
-                  trackEvent("click", "camera-details", "camera-orientation", cam.name);
-                }
-              }}>
-
-              {unavailable && 
-                <div className="unavailable-overlay">
-                  <FontAwesomeIcon className="icon" icon={faVideoSlash} />
-                </div>
-              }
-
-              <img
-                ref={imageRef}
-                src={cam.links.imageDisplay}
-                alt={cam.name} />
-
-              {(stale || delayed || unavailable || updated) &&
-                <span
-                  className={'status-pill' + (stale ? ' stale' : '') + (delayed ? ' delayed' : '') + (unavailable ? ' unavailable' : '') + (updated ? ' updated' : '')}>
-                  {getStateMessage()}
-                </span>
-              }
-
-              <span
-                aria-label={getCameraOrientation(cam.orientation)}
-                className={'label' + (camera.id === cam.id ? ' current' : '')}>
-
-                {getFullOrientation(cam.orientation)}
-              </span>
-            </div>
+        <div className={'main-content' + ' camCount' + camData.camGroup.length}>
+          {camData.camGroup.map((cam, index) => (
+            <CameraThumbnail key={cam.id} thumbnailCamera={cam} mainCamera={camData.camGroup[currentCamIndex]} index={index} switchOrientation={switchOrientation} />
           ))}
         </div>
       }
