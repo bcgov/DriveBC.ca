69fc8818184c4477a83e72bc26a3718c6f84f859
Mulder <Willem.Mulder@gov.bc.ca>
2026-01-30 09:21:50 -0800

    DBC22-5537: Remove old staledelayed endpoint

--- src/backend/apps/webcam/views.py
+++ src/backend/apps/webcam/views.py
@@ -280,26 +280,6 @@ class CameraViewSet(WebcamAPI, viewsets.ReadOnlyModelViewSet):
         response['Content-Disposition'] = 'attachment; filename="images.zip"'
         return response
 
-    def _staleAndDelayed(self, request, pk=None):
-        countStale = Webcam.objects.filter(should_appear=True, marked_stale=True).count()
-        countDelayed = Webcam.objects.filter(should_appear=True, marked_delayed=True).count()
-        totalCams = Webcam.objects.filter(should_appear=True).count()
-        host = request.get_host()
-        path = reverse('webcams-staleAndDelayed')
-        self_link = f"{request.scheme}://{host}{path}"
-
-        data = {
-            "links": {
-                "self": self_link
-            },
-            "time": timezone.now().strftime("%Y-%m-%d %H:%M:%S"),
-            "cams": totalCams,
-            "stale": countStale,
-            "delayed": countDelayed,
-        }
-
-        return Response(data)
-
     # Admin-only endpoint
     @action(
         detail=True,
@@ -349,18 +329,6 @@ class CameraViewSet(WebcamAPI, viewsets.ReadOnlyModelViewSet):
             return Response({"detail": "Admin access required."}, status=status.HTTP_403_FORBIDDEN)
         return self._download(request, pk)
 
-    @action(
-            detail=False, 
-            methods=['get'], 
-            url_path='admin-staleAndDelayed',
-            permission_classes=[IsAdminUser],
-            )
-    def staleAndDelayed_admin(self, request, pk=None):
-        user = request.user
-        if not user.is_authenticated or not user.is_staff:
-            return Response({"detail": "Admin access required."}, status=status.HTTP_403_FORBIDDEN)
-        return self._staleAndDelayed(request, pk)
-
     # Public proxy endpoint
     @action(
         detail=True,
@@ -403,9 +371,6 @@ class CameraViewSet(WebcamAPI, viewsets.ReadOnlyModelViewSet):
             url_path='staleAndDelayed',
             permission_classes=[AllowAny],
             )
-    def staleAndDelayed_public(self, request, pk=None):
-        # forward to the real admin logic
-        return self._staleAndDelayed(request, pk)
 
     @staticmethod
     def is_stale(obj, now=None):
