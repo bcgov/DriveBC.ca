889bfb3058a133ff7d75de669c518c516e5b6df1
bcgov-brwang <87880048+bcgov-brwang@users.noreply.github.com>
2026-03-17 10:43:16 -0700

    DBC22-5879: made message_display/1/2/3 read only

--- src/backend/apps/dms/admin.py
+++ src/backend/apps/dms/admin.py
@@ -3,6 +3,6 @@ from django.contrib.gis import admin
 
 
 class DmsAdmin(admin.GISModelAdmin):
-    readonly_fields = ('id', )
+    readonly_fields = ('id', 'message_display_1', 'message_display_2', 'message_display_3')
 
 admin.site.register(Dms, DmsAdmin)
