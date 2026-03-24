25e3203d0f4178fa6082a81db80205438d2b16b4
minORC <min@openroad.ca>
2026-03-24 15:37:30 -0700

    DBC22-5365: Added external URL icon for external links throughout the site. (#1257)

--- src/frontend/src/Components/cameras/nearbyweathers/NearbyRegionalWeather.js
+++ src/frontend/src/Components/cameras/nearbyweathers/NearbyRegionalWeather.js
@@ -66,6 +66,7 @@ export default function NearbyRegionalWeather(props) {
                 {event.Url &&
                   <p className="label link">
                     <a
+                      className="environment-canada-link"
                       alt="Environment Canada Details Link"
                       rel="noreferrer"
                       href={event.Url}>
