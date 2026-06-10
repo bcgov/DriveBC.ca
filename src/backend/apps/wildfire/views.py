6e7882e301b6e847d07a40324423e5cc750b9631
ray <ray@oxd.com>
2026-06-10 15:32:59 -0700

    DBC22-6514: show wildfires that are "Being Held"

--- src/backend/apps/wildfire/views.py
+++ src/backend/apps/wildfire/views.py
@@ -1,15 +1,16 @@
-from apps.wildfire.models import Wildfire
-from apps.wildfire.serializers import WildfireSerializer
 from apps.shared.enums import CacheKey, CacheTimeout
 from apps.shared.views import CachedListModelMixin
+from apps.wildfire.models import Wildfire
+from apps.wildfire.serializers import WildfireSerializer
 from rest_framework import viewsets
 
 
 class WildfireAPI(CachedListModelMixin):
-    queryset = Wildfire.objects.filter(status='Out of Control')
+    queryset = Wildfire.objects.filter(status__in=['Out of Control', 'Being Held'])
     serializer_class = WildfireSerializer
     cache_key = CacheKey.WILDFIRE_LIST
     cache_timeout = CacheTimeout.WILDFIRE_LIST
+
 
 class WildfireViewSet(WildfireAPI, viewsets.ReadOnlyModelViewSet):
     pass
