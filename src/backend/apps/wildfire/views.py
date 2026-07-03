c89fc1c843eea7858c9205a5fa28c8e5dc4942c3
ray <ray@oxd.com>
2026-07-03 15:29:50 -0700

    DBC22-6499: update wildfire display requirements
    Show wildfires without polygon data, hide Out and Under Control statuses,
    update map legend wording, and add Fire of Note status definition in panel.

--- src/backend/apps/wildfire/views.py
+++ src/backend/apps/wildfire/views.py
@@ -1,12 +1,13 @@
 from apps.shared.enums import CacheKey, CacheTimeout
 from apps.shared.views import CachedListModelMixin
+from apps.wildfire.enums import WILDFIRE_DISPLAY_STATUSES
 from apps.wildfire.models import Wildfire
 from apps.wildfire.serializers import WildfireSerializer
 from rest_framework import viewsets
 
 
 class WildfireAPI(CachedListModelMixin):
-    queryset = Wildfire.objects.filter(status__in=['Out of Control', 'Being Held', 'Fire of Note'])
+    queryset = Wildfire.objects.filter(status__in=WILDFIRE_DISPLAY_STATUSES)
     serializer_class = WildfireSerializer
     cache_key = CacheKey.WILDFIRE_LIST
     cache_timeout = CacheTimeout.WILDFIRE_LIST
