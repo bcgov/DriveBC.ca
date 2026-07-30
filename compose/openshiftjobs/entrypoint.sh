76a9afc980062809873f348a52c6ff8c6922605a
wmuldergov <willem.mulder@gov.bc.ca>
2026-07-30 11:21:16 -0700

    remove scripts

--- compose/openshiftjobs/entrypoint.sh
+++ compose/openshiftjobs/entrypoint.sh
@@ -12,14 +12,6 @@ case "$1" in
         # Run backupmediapvc.sh which will backup the data from the django-media pvc to s3 storage
         /scripts/backupmediapvc.sh
         ;;
-    ziplogs)
-        # Run ziplogs.sh which will zip all files that were created in the previous hour or older in the nginx log storage pvc
-        /scripts/ziplogs.sh
-        ;;
-    analyzeexportlogs)
-        # Run analyzeexportlogs with additional arguments which will send the specified days logs through goaccess and then upload to s3.
-        /scripts/analyzeexportlogs.sh
-        ;;
     *)
         echo "Invalid script"
         exit 1
