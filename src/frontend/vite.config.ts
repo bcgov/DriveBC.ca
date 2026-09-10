6e8f819366b534bad79ef533c867acf087b6025d
ray <ray@oxd.com>
2026-09-10 15:33:27 -0700

    DBC22-7332: enabled sourcemap for test and dev

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   },
   build: {
     outDir: 'build',
-    sourcemap: false,
+    sourcemap: process.env.SOURCEMAP !== 'false',
   },
   server: {
     host: '0.0.0.0',
